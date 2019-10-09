a0f5a094b048d7dabed38dc90c9faf9b3a930ee9
Apollo2016926 <944644308@qq.com>
2019-10-09 12:19:42 +0800

    移动零

--- src/com/eden/primary/Arrays.java
+++ src/com/eden/primary/Arrays.java
@@ -9,16 +9,7 @@ import java.util.List;
  * @date: 2019/9/25
  **/
 public class Arrays {
-    public static void main(String[] args) {
 
-        //交集
-        int[] nums1 = {1, 2, 2, 1};
-        int[] nums2 = {2, 2};
-        int[] intersect = intersect(nums1, nums2);
-        System.out.println(java.util.Arrays.toString(intersect));
-        int[] plusOne = {4, 3, 2, 1};
-        System.out.println(java.util.Arrays.toString(plusOne(plusOne)));
-    }
 
     /**
      * @Description: 给定两个数组，编写一个函数来计算它们的交集
@@ -57,6 +48,7 @@ public class Arrays {
         }
         return finalArray;
     }
+
     /**
      * @Description: 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
      * 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
@@ -83,5 +75,41 @@ public class Arrays {
             }
         }
         return digits;
+    }
+
+    /**
+     * @Description: 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+     * 示例:  输入: [0,1,0,3,12] 输出: [1,3,12,0,0]
+     * 说明:必须在原数组上操作，不能拷贝额外的数组.尽量减少操作次数。
+     * @Param: [nums]
+     * @Return: void
+     * @Author: gexx
+     * @Date: 2019/10/9
+     **/
+    public static void moveZeroes(int[] nums) {
+        int zeroNum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (0 == nums[i]) {
+                zeroNum++;
+            } else if (zeroNum != 0) {
+                nums[i - zeroNum] = nums[i];
+                nums[i] = 0;
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+
+        //交集
+        int[] nums1 = {1, 2, 2, 1};
+        int[] nums2 = {2, 2};
+        int[] intersect = intersect(nums1, nums2);
+        System.out.println(java.util.Arrays.toString(intersect));
+        int[] plusOne = {4, 3, 2, 1};
+        System.out.println(java.util.Arrays.toString(plusOne(plusOne)));
+        //移动0
+        int [] nums3={0,1,0,3,12};
+        moveZeroes(nums3);
+        System.out.println(java.util.Arrays.toString(nums3));
     }
 }
