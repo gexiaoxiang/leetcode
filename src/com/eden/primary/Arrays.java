fb64203347ce7796df324b3f03f382e637d6d5bc
Apollo2016926 <944644308@qq.com>
2019-10-10 12:42:51 +0800

    两数之和

--- src/com/eden/primary/Arrays.java
+++ src/com/eden/primary/Arrays.java
@@ -98,6 +98,31 @@ public class Arrays {
         }
 
     }
+
+    /**
+     * @Description: 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素
+     * 给定 nums = [2, 7, 11, 15], target = 9
+     * 因为 nums[0] + nums[1] = 2 + 7 = 9
+     * 所以返回 [0, 1]
+     * @Param: [nums, target]
+     * @Return: int[]
+     * @Author: gexx
+     * @Date: 2019/10/10
+     **/
+    public static int[] twoSum(int[] nums, int target) {
+        int[] re = new int[2];
+        for (int i =0; i<nums.length;i++){
+            for(int j =i+1 ;j<=nums.length-1;j++){
+               if(nums[i]+nums[j]==target){
+                   re[0]=i;
+                   re[1]=j;
+                   return re;
+            }
+        }}
+
+        return re;
+    }
+
     public static void main(String[] args) {
 
         //交集
@@ -108,8 +133,14 @@ public class Arrays {
         int[] plusOne = {4, 3, 2, 1};
         System.out.println(java.util.Arrays.toString(plusOne(plusOne)));
         //移动0
-        int [] nums3={0,1,0,3,12};
+        int[] nums3 = {0, 1, 0, 3, 12};
         moveZeroes(nums3);
         System.out.println(java.util.Arrays.toString(nums3));
+        //两数之和
+        int[] nums4 = {3, 2, 4};
+        int target = 6;
+
+        int[] ints = twoSum(nums4, target);
+        System.out.println(java.util.Arrays.toString(ints));
     }
 }
