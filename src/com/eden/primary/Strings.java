e0621d2760ffd57ad933f7111b87335c23333fac
Apollo2016926 <944644308@qq.com>
2019-10-15 12:48:33 +0800

    整数反转

--- src/com/eden/primary/Strings.java
+++ src/com/eden/primary/Strings.java
@@ -20,8 +20,8 @@ public class Strings {
      **/
     public static void reverseString(char[] s) {
 
-        for (int i = 0; i < s.length/ 2; i++) {
-                char mid = s[i];
+        for (int i = 0; i < s.length / 2; i++) {
+            char mid = s[i];
             s[i] = s[s.length - 1 - i];
             s[s.length - 1 - i] = mid;
         }
@@ -29,9 +29,46 @@ public class Strings {
 
     }
 
+    /**
+     * @Description: 整数反转
+     * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+     * 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+     * @Param: [x]
+     * @Return: int
+     * @Author: gexx
+     * @Date: 2019/10/15
+     **/
+    public static int reverse(int x) {
+        String s = String.valueOf(x);
+        StringBuffer stringBuffer = new StringBuffer(s.length());
+        stringBuffer.append(x);
+        if (x < 0) {
+            stringBuffer.deleteCharAt(0);
+            stringBuffer.reverse();
+            stringBuffer.insert(0, '-');
+        } else {
+            stringBuffer.reverse();
+        }
+        long sss = Long.valueOf(stringBuffer.toString());
+
+        if (sss >= Integer.MAX_VALUE ||
+                sss <= Integer.MIN_VALUE) {
+            return 0;
+        }
+        Integer re = Integer.valueOf(stringBuffer.toString());
+
+        return re;
+    }
+
     public static void main(String[] args) {
-        //
+        //反转字符串
         char[] s = {'h', 'e', 'l', 'l', 'o'};
         reverseString(s);
+        //整数反转
+//        int x=9646324351;
+        int x = 123;
+        int reverse = reverse(x);
+        System.out.println(reverse);
     }
+
 }
