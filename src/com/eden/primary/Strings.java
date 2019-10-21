19ab7ed12ccb63affaa5a3885d366419bad063cc
Apollo2016926 <944644308@qq.com>
2019-10-21 12:17:45 +0800

    字符串转换整数

--- src/com/eden/primary/Strings.java
+++ src/com/eden/primary/Strings.java
@@ -137,6 +137,55 @@ public class Strings {
         return str.toString().equals(str.reverse().toString());
     }
 
+    /**
+     * @Description: 字符串转换整数 (atoi)
+     * 请你来实现一个 atoi 函数，使其能将字符串转换成整数。
+     * 首先，该函数会根据需要丢弃无用的开头空格字符，直到寻找到第一个非空格的字符为止。
+     * 当我们寻找到的第一个非空字符为正或者负号时，则将该符号与之后面尽可能多的连续数字组合起来，作为该整数的正负号；假如第一个非空字符是数字，则直接将其与之后连续的数字字符组合起来，形成整数。
+     * 该字符串除了有效的整数部分之后也可能会存在多余的字符，这些字符可以被忽略，它们对于函数不应该造成影响。
+     * 注意：假如该字符串中的第一个非空格字符不是一个有效整数字符、字符串为空或字符串仅包含空白字符时，则你的函数不需要进行转换。
+     * 在任何情况下，若函数不能进行有效的转换时，请返回 0。
+     * @Param: [str]
+     * @Return: int
+     * @Author: gexx
+     * @Date: 2019/10/21
+     **/
+    public static int myAtoi(String str) {
+        int n = str.length();
+        int i = 0;
+        while (i < n && str.charAt(i) == ' ') {
+            i++;
+        }
+        if (i == n || !((str.charAt(i) == '+') || (str.charAt(i) == '-') || (str.charAt(i) >= '0' && str.charAt(i) <= '9'))) {
+            return 0;
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        if (str.charAt(i) == '-') {
+            stringBuilder.append('-');
+            i++;
+        } else if (str.charAt(i) == '+') {
+            i++;
+        }
+        if (i == n || !(str.charAt(i) >= '0' && str.charAt(i) <= '9')) {
+            return 0;
+        }
+        while (i < n && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+            stringBuilder.append(str.charAt(i));
+            i++;
+        }
+        try {
+            return Integer.valueOf(stringBuilder.toString());
+        } catch (Exception e) {
+            if (stringBuilder.substring(0, 1).equals("-")) {
+                return Integer.MIN_VALUE;
+            } else {
+                return Integer.MAX_VALUE;
+            }
+        }
+
+
+    }
+
     public static void main(String[] args) {
         //反转字符串
         char[] s = {'h', 'e', 'l', 'l', 'o'};
@@ -157,6 +206,10 @@ public class Strings {
         String palindromeStr = "A man, a plan, a canal: Panama";
 //        String palindromeStr = "race a car";
         System.out.println(isPalindrome(palindromeStr));
+        //字符串转换整数
+        String myAtoi = "";
+        System.out.println(myAtoi(myAtoi));
+
     }
 
 }
