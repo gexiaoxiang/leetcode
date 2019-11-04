3610e2850ee0ac01bb698dac8b1a4b75d5adaccc
Apollo2016926 <944644308@qq.com>
2019-11-04 12:19:55 +0800

    合并两个有序链表

--- src/com/eden/primary/Linkeds.java
+++ src/com/eden/primary/Linkeds.java
@@ -86,4 +86,29 @@ public class Linkeds {
         return head;
     }
 
+    /**
+     * @Description: 合并两个有序链表
+     * @Param: [l1, l2]
+     * @Return: com.eden.primary.Linkeds.ListNode
+     * @Author: gexx
+     * @Date: 2019/11/4
+     **/
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        ListNode dummyHead = new ListNode(0);
+        ListNode lastNode = dummyHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                lastNode.next = l1;
+                l1 = l1.next;
+            } else {
+                lastNode.next = l2;
+                l2 = l2.next;
+            }
+
+            lastNode = lastNode.next;
+        }
+        lastNode.next = l1 != null ? l1 : l2;
+        return dummyHead.next;
+    }
 }
