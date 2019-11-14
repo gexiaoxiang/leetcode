5d224583c9fb4947d7ae9bab6095cb5ba5c83212
Apollo2016926 <944644308@qq.com>
2019-11-14 12:24:10 +0800

    买卖股票的最佳时机

--- src/com/eden/primary/DynamicProgramming.java
+++ src/com/eden/primary/DynamicProgramming.java
@@ -16,19 +16,54 @@ public class DynamicProgramming {
      **/
     public int climbStairs(int n) {
 
-        if(n<=2){
+        if (n <= 2) {
             return n;
         }
-        int cur=2;//到达当前阶梯的方法数，从2开始，与for循环i对应
-        int pre=1;//到达前一个阶梯的方法数
-        for(int i=2;i<n;i++){
-            int next=pre+cur;//到达后一个阶梯的方法数
-            pre=cur;
-            cur=next;//最后一躺循环i=n-1;next=dp(i-1)+dp(i)赋值给cur，然后i++,i=n跳出循环
+        int cur = 2;//到达当前阶梯的方法数，从2开始，与for循环i对应
+        int pre = 1;//到达前一个阶梯的方法数
+        for (int i = 2; i < n; i++) {
+            int next = pre + cur;//到达后一个阶梯的方法数
+            pre = cur;
+            cur = next;//最后一躺循环i=n-1;next=dp(i-1)+dp(i)赋值给cur，然后i++,i=n跳出循环
 
         }
         return cur;
 
+
+    }
+
+    /**
+     * @Description: 买卖股票的最佳时机
+     * @Param: [prices]
+     * @Return: int
+     * @Author: gexx
+     * @Date: 2019/11/14
+     **/
+    public static int maxProfit(int[] prices) {
+
+        int max = 0;
+        if (prices.length < 2) {
+            return max;
+        }
+        if (prices.length == 2 && prices[1] - prices[0] > 0) {
+            return prices[1] - prices[0];
+        }
+        for (int in = 0; in < prices.length - 1; in++) {
+            for (int out = in + 1; out < prices.length; out++) {
+                int difference = prices[out] - prices[in];
+                if (max < difference) {
+                    max = difference;
+                }
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int prices[] = {1, 2, 4};
+        int max = maxProfit(prices);
+        System.out.println(max);
 
     }
 }
