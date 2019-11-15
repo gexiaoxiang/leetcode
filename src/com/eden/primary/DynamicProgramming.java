0ff690fdcc0e760081042a04263be3f0443c16c2
Apollo2016926 <944644308@qq.com>
2019-11-15 12:20:29 +0800

    最大子序和

--- src/com/eden/primary/DynamicProgramming.java
+++ src/com/eden/primary/DynamicProgramming.java
@@ -60,10 +60,34 @@ public class DynamicProgramming {
         return max;
     }
 
+    /**
+     * @Description: 最大子序和
+     * @Param: [nums]
+     * @Return: int
+     * @Author: gexx
+     * @Date: 2019/11/15
+     **/
+    public static int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int local = nums[0];
+        int global = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            local = Math.max(nums[i], local + nums[i]);
+            global = Math.max(local, global);
+        }
+        return global;
+    }
+
     public static void main(String[] args) {
         int prices[] = {1, 2, 4};
         int max = maxProfit(prices);
         System.out.println(max);
+
+        int nums[]={-2,1,-3,4,-1,2,1,-5,4};
+        int maxSubArray= maxSubArray(nums);
+        System.out.println(maxSubArray);
 
     }
 }
