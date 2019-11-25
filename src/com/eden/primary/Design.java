cbd7224666e1225eca730b47d1dd7493dcfe114c
Apollo2016926 <944644308@qq.com>
2019-11-25 12:03:05 +0800

    最小栈

--- src/com/eden/primary/Design.java
+++ src/com/eden/primary/Design.java
@@ -1,8 +1,8 @@
 package com.eden.primary;
 
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Random;
+import java.util.Stack;
 
 /**
  * @Description: 设计问题
@@ -42,6 +42,45 @@ public class Design {
         return copy;
     }
     }
+
+    /**
+     * 最小栈
+     */
+    static class MinStack {
+        private Stack<Integer> stack1;
+        private Stack<Integer> stack2;
+
+        /** initialize your data structure here. */
+        public MinStack() {
+            this.stack1 = new Stack<Integer>();
+            this.stack2 = new Stack<Integer>();
+        }
+
+        public void push(int x) {
+            this.stack1.push(x);
+            if(this.stack2.isEmpty()){
+                this.stack2.push(x);
+            }else if(x < this.stack2.peek()){
+                this.stack2.push(x);
+            }else{
+                this.stack2.push(stack2.peek());
+            }
+        }
+
+        public void pop() {
+            this.stack1.pop();
+            this.stack2.pop();
+        }
+
+        public int top() {
+            return this.stack1.peek();
+        }
+
+        public int getMin() {
+            return this.stack2.peek();
+        }
+    }
+
 
     public static void main(String[] args) {
         int[] nums = {1, 2, 3};
