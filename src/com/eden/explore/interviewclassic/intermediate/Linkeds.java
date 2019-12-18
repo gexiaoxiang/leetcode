4f7efc61568c7a38ffcb6267bdca7a26b1f19c1d
Apollo2016926 <944644308@qq.com>
2019-12-18 12:05:04 +0800

    奇偶链表

--- src/com/eden/explore/interviewclassic/intermediate/Linkeds.java
+++ src/com/eden/explore/interviewclassic/intermediate/Linkeds.java
@@ -55,20 +55,40 @@ public class Linkeds {
 
     }
 
-    public static void main(String[] args) {
-        ListNode l1=new ListNode(2);
-        ListNode l2=new ListNode(4);
-        ListNode l3=new ListNode(3);
-        l1.next=l2;
-        l2.next=l3;
-        l3.next=null;
+    /**
+     * @Description: 奇偶链表
+     * @Param: [head]
+     * @Return: com.eden.explore.interviewclassic.intermediate.Linkeds.ListNode
+     * @Author: gexx
+     * @Date: 2019/12/18
+     **/
+    public static ListNode oddEvenList(ListNode head) {
+        if(head==null) return head;
+        ListNode odd=head,even=head.next,evenhead=even;
+        while(odd.next!=null&&even.next!=null){
+            odd.next=odd.next.next;
+            even.next=even.next.next;
+            odd=odd.next;
+            even=even.next;
+        }
+        odd.next=evenhead;
+        return head;
+    }
 
-        ListNode l4=new ListNode(5);
-        ListNode l5=new ListNode(6);
-        ListNode l6=new ListNode(4);
-        l4.next=l5;
-        l5.next=l6;
-        l6.next=null;
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        ListNode l2 = new ListNode(4);
+        ListNode l3 = new ListNode(3);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = null;
+
+        ListNode l4 = new ListNode(5);
+        ListNode l5 = new ListNode(6);
+        ListNode l6 = new ListNode(4);
+        l4.next = l5;
+        l5.next = l6;
+        l6.next = null;
         ListNode listNode = addTwoNumbers(l1, l4);
         System.out.print(listNode);
     }
