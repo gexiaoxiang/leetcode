bcf31844382b1ab61e7f4a4de08a6a036b007212
Apollo2016926 <944644308@qq.com>
2020-01-02 12:10:41 +0800

    两数之和 II - 输入有序数组

--- src/main/java/com/eden/explore/datastructure/arrayandstring/DoublePointer.java
+++ src/main/java/com/eden/explore/datastructure/arrayandstring/DoublePointer.java
@@ -28,9 +28,33 @@ public class DoublePointer {
 
     }
 
+    /**
+     * @Description: 两数之和 II - 输入有序数组
+     * @Param: [numbers, target]
+     * @Return: int[]
+     * @Author: gexx
+     * @Date: 2020/1/2
+     **/
+    public static int[] twoSum(int[] numbers, int target) {
+        int i = 0;
+        int j = numbers.length - 1;
+        while (i < j) {
+            if (numbers[i] + numbers[j] > target) {
+                j--;
+            } else if (numbers[i] + numbers[j] < target) {
+                i++;
+            } else {
+                return new int[]{i + 1, j + 1};
+            }
+        }
+        return new int[]{};
+    }
+
     public static void main(String[] args) {
-        int []nums={1,4,3,2};
-        System.out.println( arrayPairSum(nums));
+        int[] nums = {1, 4, 3, 2};
+        System.out.println(arrayPairSum(nums));
+        int[] numbers={2, 7, 11, 15};
+        System.out.println(Arrays.toString(twoSum(numbers,9)));
     }
 
 }
