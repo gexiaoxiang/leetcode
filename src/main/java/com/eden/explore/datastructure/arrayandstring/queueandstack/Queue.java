5a78767cd2df4c624f15f590678b4b7e29b49da5
Apollo2016926 <944644308@qq.com>
2020-01-09 12:31:18 +0800

    队列：先入先出的数据结构

---
+++ src/main/java/com/eden/explore/datastructure/arrayandstring/queueandstack/Queue.java
@@ -0,0 +1,10 @@
+package com.eden.explore.datastructure.arrayandstring.queueandstack;
+
+/**
+ * @Description: 队列：先入先出的数据结构
+ * @Author gexx
+ * @Date 2020/1/9
+ * @Version V1.0
+ **/
+public class Queue {
+}
