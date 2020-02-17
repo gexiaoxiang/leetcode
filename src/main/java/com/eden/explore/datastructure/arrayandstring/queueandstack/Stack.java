3014955f9c1079b11eb9806a9dac2456d3d2ef3e
gexx <944644308@qq.com>
2020-02-17 22:48:30 +0800

    岛屿数量-深度優先

--- src/main/java/com/eden/explore/datastructure/arrayandstring/queueandstack/Stack.java
+++ src/main/java/com/eden/explore/datastructure/arrayandstring/queueandstack/Stack.java
@@ -91,31 +91,79 @@ public class Stack {
      * @Date 2020/2/15
      **/
     public static int evalRPN(String[] tokens) {
-        java.util.Stack<Integer> stack=new  java.util.Stack<Integer>();
-        int num1=0,num2=0;
+        java.util.Stack<Integer> stack = new java.util.Stack<Integer>();
+        int num1 = 0, num2 = 0;
         for (int i = 0; i < tokens.length; i++) {
-            String s=tokens[i];
-            if(s.equals("+")){
-                num1=stack.pop();
-                num2=stack.pop();
-                stack.push(num2+num1);
-            }else if(s.equals("-")){
-                num1=stack.pop();
-                num2=stack.pop();
-                stack.push(num2-num1);
-            }else if(s.equals("*")){
-                num1=stack.pop();
-                num2=stack.pop();
-                stack.push(num2*num1);
-            }else if(s.equals("/")){
-                num1=stack.pop();
-                num2=stack.pop();
-                stack.push(num2/num1);
-            }else{
+            String s = tokens[i];
+            if (s.equals("+")) {
+                num1 = stack.pop();
+                num2 = stack.pop();
+                stack.push(num2 + num1);
+            } else if (s.equals("-")) {
+                num1 = stack.pop();
+                num2 = stack.pop();
+                stack.push(num2 - num1);
+            } else if (s.equals("*")) {
+                num1 = stack.pop();
+                num2 = stack.pop();
+                stack.push(num2 * num1);
+            } else if (s.equals("/")) {
+                num1 = stack.pop();
+                num2 = stack.pop();
+                stack.push(num2 / num1);
+            } else {
                 stack.push(Integer.parseInt(s));
             }
         }
         return stack.pop();
+    }
+
+    /**
+     * @Description 岛屿数量-深度優先
+     * @Author gexx
+     * @Date 2020/2/17
+     **/
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        if (grid.length == 0) {
+            return 0;
+        }
+        int row = grid.length;
+        int l = grid[0].length;
+        char[][] vist = new char[row][l];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < l; j++) {
+                if (vist[i][j] != '1' && grid[i][j] == '1') //如果没有访问过，且为陆地，计数加1。
+                {
+                    count++;
+                }
+                FS(grid, vist, i, j); //遍历这个区域周围，将与自己相连的区域 vist全置为1(相连接的 1 为一块)
+            }
+            //继续下一个位子
+        }
+        return count;
+    }
+
+    //将与这个1相连所有区域全部置为访问过。
+    public static void FS(char[][] grid, char[][] vist, int i, int j) {
+        if (i < 0 || i > grid.length - 1 || j < 0 || j > grid[0].length - 1) {
+            return;
+        }
+        if (grid[i][j] == '0') { //访问到海洋返回
+            return;
+        }
+        if (vist[i][j] == '1') { //访问过的陆地 返回。
+            return;
+        }
+        //没访问过 先置1
+        vist[i][j] = '1';
+        //上下左右遍历
+        FS(grid, vist, i + 1, j);
+        FS(grid, vist, i - 1, j);
+        FS(grid, vist, i, j + 1);
+        FS(grid, vist, i, j - 1);
+        return;
+
     }
 
     public static void main(String[] args) {
