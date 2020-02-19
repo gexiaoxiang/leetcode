b4f2ae32aa43192ccf59855fec257bfc1a62d903
gexx <944644308@qq.com>
2020-02-19 22:52:29 +0800

    目标和

--- src/main/java/com/eden/explore/datastructure/arrayandstring/queueandstack/Stack.java
+++ src/main/java/com/eden/explore/datastructure/arrayandstring/queueandstack/Stack.java
@@ -208,6 +208,33 @@ public class Stack {
     }
 
 
+    /**
+     * @Description 目标和
+     * @Author gexx
+     * @Date 2020/2/19
+     **/
+
+    public static int findTargetSumWays(int[] nums, int S) {
+
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum < S || (sum + S) % 2 == 1) {
+            return 0;
+        }
+        int w = (sum + S) / 2;
+        int[] dp = new int[w + 1];
+        dp[0] = 1;
+        for (int num : nums) {
+            for (int j = w; j >= num; j--) {
+                dp[j] += dp[j - num];
+            }
+        }
+        return dp[w];
+
+    }
+
     public static void main(String[] args) {
         int temperatures[] = {73, 74, 75, 71, 69, 72, 76, 73};
         System.out.println(Arrays.toString(dailyTemperatures(temperatures)));
