9bb7cdee85df37e788461d882402fe82954e0d08
gexx <944644308@qq.com>
2020-02-24 22:30:31 +0800

    图像渲染-2

--- src/main/java/com/eden/explore/datastructure/arrayandstring/queueandstack/Summary.java
+++ src/main/java/com/eden/explore/datastructure/arrayandstring/queueandstack/Summary.java
@@ -137,10 +137,28 @@ public class Summary {
      * @author gexx
      * @Date 2020/2/23
      **/
-
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-
-        return new int[][]{};
+        int target = image[sr][sc];
+        dfs(image, newColor, sr, sc, target);
+        return image;
     }
 
+    private void dfs(int[][] image, int newColor, int i, int j, int target) {
+        if (image[i][j] == newColor || image[i][j] != target) {
+            return;
+        }
+        image[i][j] = newColor;
+        if (i > 0) {
+            dfs(image, newColor, i - 1, j, target);
+        }
+        if (i < image.length - 1) {
+            dfs(image, newColor, i + 1, j, target);
+        }
+        if (j > 0) {
+            dfs(image, newColor, i, j - 1, target);
+        }
+        if (j < image[i].length - 1) {
+            dfs(image, newColor, i, j + 1, target);
+        }
+    }
 }
