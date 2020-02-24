164d81b4a3af6ea6aee68b2d617f1e636b59b53a
gexx <944644308@qq.com>
2020-02-25 00:34:44 +0800

    01 矩阵

--- src/main/java/com/eden/explore/datastructure/arrayandstring/queueandstack/Summary.java
+++ src/main/java/com/eden/explore/datastructure/arrayandstring/queueandstack/Summary.java
@@ -137,13 +137,13 @@ public class Summary {
      * @author gexx
      * @Date 2020/2/23
      **/
-    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+    public static int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
         int target = image[sr][sc];
         dfs(image, newColor, sr, sc, target);
         return image;
     }
 
-    private void dfs(int[][] image, int newColor, int i, int j, int target) {
+    private static void dfs(int[][] image, int newColor, int i, int j, int target) {
         if (image[i][j] == newColor || image[i][j] != target) {
             return;
         }
@@ -160,5 +160,41 @@ public class Summary {
         if (j < image[i].length - 1) {
             dfs(image, newColor, i, j + 1, target);
         }
+    }
+
+    /**
+     * @Description 01 矩阵
+     * @author gexx
+     * @Date 2020/2/24
+     **/
+    public static int[][] updateMatrix(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        // 第一次遍历，正向遍历，根据相邻左元素和上元素得出当前元素的对应结果
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == 1) {
+                    matrix[i][j] = row + col;
+                }
+                if (i > 0) {
+                    matrix[i][j] = Math.min(matrix[i][j], matrix[i - 1][j] + 1);
+                }
+                if (j > 0) {
+                    matrix[i][j] = Math.min(matrix[i][j], matrix[i][j - 1] + 1);
+                }
+            }
+        }
+        // 第二次遍历，反向遍历，根据相邻右元素和下元素及当前元素的结果得出最终结果
+        for (int i = row - 1; i >= 0; i--) {
+            for (int j = col - 1; j >= 0; j--) {
+                if (i < row - 1) {
+                    matrix[i][j] = Math.min(matrix[i][j], matrix[i + 1][j] + 1);
+                }
+                if (j < col - 1) {
+                    matrix[i][j] = Math.min(matrix[i][j], matrix[i][j + 1] + 1);
+                }
+            }
+        }
+        return matrix;
     }
 }
