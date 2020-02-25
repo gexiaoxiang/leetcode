c695e0089ed5c063848ff943400925134dc88628
Apollo2016926 <944644308@qq.com>
2020-02-25 14:53:31 +0800

    复制带随机指针的链表

--- src/main/java/com/eden/explore/datastructure/linkedlist/Summary.java
+++ src/main/java/com/eden/explore/datastructure/linkedlist/Summary.java
@@ -42,7 +42,58 @@ public class Summary {
 
         }
 
+    }
+
+    /**
+     * 复制带随机指针的链表
+     */
+    class Solution2 {
+        class Node {
+            int val;
+            Node next;
+            Node random;
+
+            public Node(int val) {
+                this.val = val;
+                this.next = null;
+                this.random = null;
+            }
+        }
+
+        public Node copyRandomList(Node head) {
+            if (head == null) {
+                return null;
+            }
+
+            Node ptr = head;
+            while (ptr != null) {
+
+                // Cloned node
+                Node newNode = new Node(ptr.val);
+
+                newNode.next = ptr.next;
+                ptr.next = newNode;
+                ptr = newNode.next;
+            }
+            ptr = head;
+            while (ptr != null) {
+                ptr.next.random = (ptr.random != null) ? ptr.random.next : null;
+                ptr = ptr.next.next;
+            }
+            Node ptr_old_list = head; // A->B->C
+            Node ptr_new_list = head.next; // A'->B'->C'
+            Node head_old = head.next;
+            while (ptr_old_list != null) {
+                ptr_old_list.next = ptr_old_list.next.next;
+                ptr_new_list.next = (ptr_new_list.next != null) ? ptr_new_list.next.next : null;
+                ptr_old_list = ptr_old_list.next;
+                ptr_new_list = ptr_new_list.next;
+            }
+            return head_old;
+        }
 
     }
+
+
 }
 
