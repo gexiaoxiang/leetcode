5106f5599f054df5e35b1c4586a5a2c23cc3ed30
gexx <944644308@qq.com>
2020-03-15 18:49:47 +0800

    x 的平方根

--- src/main/java/com/eden/explore/datastructure/binarysearch/BinarySearch.java
+++ src/main/java/com/eden/explore/datastructure/binarysearch/BinarySearch.java
@@ -2,7 +2,6 @@ package com.eden.explore.datastructure.binarysearch;
 
 /**
  * @Description 二分查找
- *
  * @Author gexx
  * @Date 2020/3/13
  **/
@@ -10,7 +9,6 @@ public class BinarySearch {
 
     /**
      * @Description 二分查找
-     *
      * @author gexx
      * @Date 2020/3/13
      **/
@@ -19,11 +17,10 @@ public class BinarySearch {
         int right = nums.length - 1;
 
         while (left <= right) {
-            int mid = left + (right-left)/2;
+            int mid = left + (right - left) / 2;
             if (nums[mid] >= target) {   // ==更改的地方== 即发现第一个=的时候
                 right = mid - 1;
-            }
-            else {
+            } else {
                 left = mid + 1;
             }
         }
@@ -34,10 +31,33 @@ public class BinarySearch {
         return -1;
     }
 
+
+    /**
+     * @Description x 的平方根
+     *
+     * @author gexx
+     * @Date 2020/3/15
+     **/
+    public static int mySqrt(int x) {
+        int low = 0;
+        int high = x;
+        while (low <= high) {
+            long mid = (long) (low + high) / 2;
+            if (mid * mid < x)
+                low = (int) mid + 1;
+            else if (mid * mid > x)
+                high = (int) mid - 1;
+            else
+                return (int) mid;
+        }
+        return high;
+    }
+
+
     public static void main(String[] args) {
-       int [] nums = {-1,0,3,5,9,12};
-    int target = 9;
-        search(nums,target);
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        int target = 9;
+        search(nums, target);
     }
 
 }
