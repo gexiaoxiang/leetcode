79eace9da75bd7c97451cea94113906e2efb4ed8
gexx <944644308@qq.com>
2020-04-01 00:21:50 +0800

    分割数组的最大值

--- src/main/java/com/eden/explore/datastructure/binarysearch/BinarySearch.java
+++ src/main/java/com/eden/explore/datastructure/binarysearch/BinarySearch.java
@@ -322,7 +322,6 @@ public class BinarySearch {
 
     /**
      * @Description 找出第 k 小的距离对
-     *
      * @author gexx
      * @Date 2020/4/1
      **/
@@ -377,6 +376,48 @@ public class BinarySearch {
         return num >= k;
     }
 
+    /**
+     * @Description 分割数组的最大值
+     * @author gexx
+     * @Date 2020/4/1
+     **/
+    public int splitArray(int[] nums, int m) {
+        long left = 0, right = 0;
+        for (int n : nums) {
+            right += n;
+        }
+        if (m == 1) {
+            return (int) right;
+        }
+        long result = 0;
+        long mid;
+        while (left <= right) {
+            mid = left + right >> 1;
+            if (judge(mid, nums, m)) {
+                result = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return (int) result;
+    }
+
+    private boolean judge(long mid, int[] nums, int m) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > mid) {
+                return false;
+            }
+            if (sum + nums[i] > mid) {
+                sum = nums[i];
+                m--;
+            } else {
+                sum += nums[i];
+            }
+        }
+        return m >= 1;
+    }
 
     public static void main(String[] args) {
         int[] nums = {-1, 0, 3, 5, 9, 12};
