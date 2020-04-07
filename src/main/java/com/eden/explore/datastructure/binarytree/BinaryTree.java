a75a7aaeb58429cb37c818d16c1f1439a540b654
Apollo2016926 <944644308@qq.com>
2020-04-07 12:07:40 +0800

    路径总和

--- src/main/java/com/eden/explore/datastructure/binarytree/BinaryTree.java
+++ src/main/java/com/eden/explore/datastructure/binarytree/BinaryTree.java
@@ -28,17 +28,34 @@ public class BinaryTree {
      * @return
      */
     public static List<Integer> preorderTraversal(TreeNode root) {
-            List<Integer> list=new ArrayList<>();
-            if(root==null) return list;
-            recursion(root,list);
-            return list;
+        List<Integer> list = new ArrayList<>();
+        if (root == null) return list;
+        recursion(root, list);
+        return list;
+    }
+
+    public static void recursion(TreeNode root, List<Integer> list) {
+        if (root == null) return;
+        else {
+            list.add(root.val);
+            recursion(root.left, list);
+            recursion(root.right, list);
         }
-        public static void recursion(TreeNode root,List<Integer> list){
-            if(root==null) return;
-            else{
-                list.add(root.val);
-                recursion(root.left,list);
-                recursion(root.right,list);
-            }
-        }
+    }
+
+    /**
+     * 路径总和
+     *
+     * @param root
+     * @param sum
+     * @return
+     */
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null)
+            return false;
+        sum = sum - root.val;
+        if (root.left == null && root.right == null)
+            return (sum == 0);
+        return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
+    }
 }
