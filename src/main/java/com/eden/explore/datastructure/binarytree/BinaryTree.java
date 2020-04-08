1f0f35b8d3468c8577648f0f8c57069c054940ba
Apollo2016926 <944644308@qq.com>
2020-04-08 14:04:58 +0800

    从中序与后序遍历序列构造二叉树

--- src/main/java/com/eden/explore/datastructure/binarytree/BinaryTree.java
+++ src/main/java/com/eden/explore/datastructure/binarytree/BinaryTree.java
@@ -58,4 +58,33 @@ public class BinaryTree {
             return (sum == 0);
         return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
     }
+
+    /**
+     * @Description: 从中序与后序遍历序列构造二叉树
+     * @Author: gexx
+     * @Date: 2020/4/8
+     **/
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+
+        int instart = 0, inend = inorder.length - 1;
+        int poststart = 0, postend = postorder.length - 1;
+        if (postorder == null || postend == -1) return null;
+        return create(inorder, instart, inend, postorder, poststart, postend);
+    }
+
+    public TreeNode create(int[] inorder, int instart, int inend, int[] postorder, int poststart, int postend) {
+        if (postend < poststart || instart > inend) return null;
+        int value = postorder[postend]; //后续遍历的最后一个节点作为根节点，记录值
+        int mid = 0;
+        for (int i = 0; i < inorder.length; i++) { //后序遍历的最后一个节点在中序遍历中的位子。
+            if (value == inorder[i]) {
+                mid = i;
+            }
+        }
+        int num = mid - instart;   //记录左子树节点个数,后面用于构造子树时 数组区间下标设置。
+        TreeNode root = new TreeNode(value);
+        root.left = create(inorder, instart, mid - 1, postorder, poststart, poststart + num - 1);
+        root.right = create(inorder, mid + 1, inend, postorder, poststart + num, postend - 1);
+        return root;
+    }
 }
