cac882a8c1fdc07f1350e612340484f12c173a46
Apollo2016926 <944644308@qq.com>
2020-04-09 12:02:19 +0800

    二叉搜索树迭代器

--- src/main/java/com/eden/explore/datastructure/binarysearchtree/BST.java
+++ src/main/java/com/eden/explore/datastructure/binarysearchtree/BST.java
@@ -1,5 +1,7 @@
 package com.eden.explore.datastructure.binarysearchtree;
 
+import java.util.LinkedList;
+
 /**
  * @Description: 二叉搜索树
  * @Author gexx
@@ -7,4 +9,39 @@ package com.eden.explore.datastructure.binarysearchtree;
  * @Version V1.0
  **/
 public class BST {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    class BSTIterator {
+        LinkedList<TreeNode> stack = new LinkedList<>();
+
+        public BSTIterator(TreeNode root) {
+            TreeNode cur = root;
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+        }
+
+
+        public int next() {
+            TreeNode n=stack.pop();
+            TreeNode cur=n.right;
+            while(cur!=null){
+                stack.push(cur);
+                cur=cur.left;
+            }
+            return n.val;
+        }
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+    }
 }
