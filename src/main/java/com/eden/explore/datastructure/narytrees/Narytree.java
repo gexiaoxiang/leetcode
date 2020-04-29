64135a1810d5dc3dcdaf1c0736ba1679e966652e
Apollo2016926 <944644308@qq.com>
2020-04-29 09:49:25 +0800

    N-ary Tree Preorder Traversal

--- src/main/java/com/eden/explore/datastructure/narytrees/Narytree.java
+++ src/main/java/com/eden/explore/datastructure/narytrees/Narytree.java
@@ -1,5 +1,8 @@
 package com.eden.explore.datastructure.narytrees;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Description: N叉树
  * @Author gexx
@@ -7,4 +10,47 @@ package com.eden.explore.datastructure.narytrees;
  * @Version V1.0
  **/
 public class Narytree {
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    /**
+     * @Description: N-ary Tree Preorder Traversal
+     * @Author: gexx
+     * @Date: 2020/4/16
+     **/
+    public List<Integer> preorder(Node root) {
+        if (root == null) {
+            return new ArrayList<Integer>();
+        }
+        List<Integer> list = new ArrayList<Integer>();
+        getValue(root, list);
+        return list;
+    }
+
+    public List<Integer> getValue(Node root, List<Integer> list) {
+        if (root == null) {
+            return null;
+        }
+        list.add(root.val);
+        if (root.children != null) {
+            for (Node n : root.children) {
+                getValue(n, list);
+            }
+        }
+        return list;
+    }
 }
