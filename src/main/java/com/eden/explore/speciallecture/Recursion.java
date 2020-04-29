398a50dec883c9b958f33f926338d7e96b019e21
Apollo2016926 <944644308@qq.com>
2020-04-29 11:19:51 +0800

    不同的二叉搜索树

--- src/main/java/com/eden/explore/speciallecture/Recursion.java
+++ src/main/java/com/eden/explore/speciallecture/Recursion.java
@@ -1,5 +1,8 @@
 package com.eden.explore.speciallecture;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @Description: 递归
  * @Author gexx
@@ -17,32 +20,84 @@ public class Recursion {
         }
     }
 
-    /**
-     * @Description: 两两交换链表中的节点
-     * @Author: gexx
-     * @Date: 2020/4/29
-     **/
 
-    public ListNode swapPairs(ListNode head) {
-        if (head == null || head.next == null)
-            return head;
-        ListNode ans = head.next;
-        ListNode temp = swapPairs(head.next.next);
-        ans.next = head;
-        head.next = temp;
-        return ans;
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+
+        /**
+         * @Description: 两两交换链表中的节点
+         * @Author: gexx
+         * @Date: 2020/4/29
+         **/
+
+        public ListNode swapPairs(ListNode head) {
+            if (head == null || head.next == null)
+                return head;
+            ListNode ans = head.next;
+            ListNode temp = swapPairs(head.next.next);
+            ans.next = head;
+            head.next = temp;
+            return ans;
+        }
+
+        /**
+         * 第K个语法符号
+         *
+         * @Author: gexx
+         * @Date: 2020/4/29
+         **/
+
+
+        public int kthGrammar(int N, int K) {
+            if (N == 1) return 0;
+            return (~K & 1) ^ kthGrammar(N - 1, (K + 1) / 2);
+        }
     }
 
     /**
-     * 第K个语法符号
-     *
+     * @Description: 不同的二叉搜索树 II
      * @Author: gexx
      * @Date: 2020/4/29
      **/
 
+    public LinkedList<TreeNode> generate_trees(int start, int end) {
+        LinkedList<TreeNode> all_trees = new LinkedList<TreeNode>();
+        if (start > end) {
+            all_trees.add(null);
+            return all_trees;
+        }
 
-    public int kthGrammar(int N, int K) {
-        if (N == 1) return 0;
-        return (~K & 1) ^ kthGrammar(N - 1, (K + 1) / 2);
+        // pick up a root
+        for (int i = start; i <= end; i++) {
+            // all possible left subtrees if i is choosen to be a root
+            LinkedList<TreeNode> left_trees = generate_trees(start, i - 1);
+
+            // all possible right subtrees if i is choosen to be a root
+            LinkedList<TreeNode> right_trees = generate_trees(i + 1, end);
+
+            // connect left and right trees to the root i
+            for (TreeNode l : left_trees) {
+                for (TreeNode r : right_trees) {
+                    TreeNode current_tree = new TreeNode(i);
+                    current_tree.left = l;
+                    current_tree.right = r;
+                    all_trees.add(current_tree);
+                }
+            }
+        }
+        return all_trees;
+    }
+
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new LinkedList<TreeNode>();
+        }
+        return generate_trees(1, n);
     }
 }
