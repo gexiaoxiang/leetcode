43030301ebec46f12bd184dade9cf374ef8b475e
Apollo2016926 <944644308@qq.com>
2020-04-30 10:22:48 +0800

    盛最多水的容器

--- src/main/java/com/eden/explore/arrayalgorithm/ArrayAlgorithm.java
+++ src/main/java/com/eden/explore/arrayalgorithm/ArrayAlgorithm.java
@@ -1,6 +1,7 @@
 package com.eden.explore.arrayalgorithm;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 /**
  * @Description: 数组类算法
@@ -53,6 +54,60 @@ public class ArrayAlgorithm {
         System.out.println(ints[ints.length - k]);
 
         return ints[ints.length - k];
+    }
+
+
+    /**
+     * @Description: 反转字符串中的元音字母
+     * @Author: gexx
+     * @Date: 2020/4/30
+     **/
+
+
+    public static String reverseVowels(String s) {
+        //定义一个集合用来存放元音字母
+        HashSet<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
+
+        int pL = 0;//左指针
+        int pR = s.length() - 1;//右指针
+        char[] result = new char[s.length()];//创建新数组
+        while (pL <= pR) {
+            if (!vowels.contains(s.charAt(pL))) {//判断左指针指向元素是否为元音字母
+                result[pL] = s.charAt(pL);//如果不是向新数组对应下标赋值该元素，左指针右移
+                pL++;
+            } else if (!vowels.contains(s.charAt(pR))) {//判断右指针指向元素是否为元音字母
+                result[pR] = s.charAt(pR);//如果不是向新数组对应下标赋值该元素，右指针左移
+                pR--;
+            } else {//当左右指针都指向元音字母，向新数组pR下标赋值为s字符串pL下标的元音字母
+                //新数组pL下标赋值为s字符串pR下标的元音字母，来实现元音反转调换
+                result[pR] = s.charAt(pL);
+                result[pL] = s.charAt(pR);
+                pL++;
+                pR--;
+            }
+        }
+        return new String(result);//最后将新数组作为String参数，返回新的字符串
+    }
+
+    /**
+     * @Description: 盛最多水的容器
+     * @Author: gexx
+     * @Date: 2020/4/30
+     **/
+    public int maxArea(int[] height) {
+        int i = 0;
+        int j = height.length - 1;
+        int res = 0;
+        while (i < j) {
+            int area = (j - i) * Math.min(height[i], height[j]);  //面积为底乘以高
+            res = Math.max(res, area);  //选出大的面积
+            if (height[i] <= height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return res;
     }
 
     public static void main(String[] args) {
