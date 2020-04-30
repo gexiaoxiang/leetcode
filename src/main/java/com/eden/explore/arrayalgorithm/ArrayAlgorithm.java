6bc68205a26ea7740fdd06bec687339b27883e15
Apollo2016926 <944644308@qq.com>
2020-04-30 10:45:50 +0800

    根据字符出现频率排序

--- src/main/java/com/eden/explore/arrayalgorithm/ArrayAlgorithm.java
+++ src/main/java/com/eden/explore/arrayalgorithm/ArrayAlgorithm.java
@@ -1,7 +1,6 @@
 package com.eden.explore.arrayalgorithm;
 
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.*;
 
 /**
  * @Description: 数组类算法
@@ -108,6 +107,36 @@ public class ArrayAlgorithm {
             }
         }
         return res;
+    }
+
+    /**
+     * @Description: 根据字符出现频率排序
+     * @Author: gexx
+     * @Date: 2020/4/30
+     **/
+    public String frequencySort(String s) {
+        Map<Character, Integer> mp = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            mp.put(c, mp.getOrDefault(c, 0) + 1);
+        }
+        List<Map.Entry<Character, Integer>> list = new ArrayList<>(mp.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<Character, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Character, Integer> o1, Map.Entry<Character, Integer> o2) {
+                return o2.getValue() - o1.getValue();
+            }
+        });
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < list.size(); i++) {
+            Map.Entry<Character, Integer> en = list.get(i);
+            char key = en.getKey();
+            int val = en.getValue();
+            for (int j = 0; j < val; j++) {
+                sb.append(key);
+            }
+        }
+        return sb.toString();
     }
 
     public static void main(String[] args) {
