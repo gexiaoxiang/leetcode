995503bc40e6a1939df9f1a7c0ecf93915f6c777
Apollo2016926 <944644308@qq.com>
2020-06-04 16:09:47 +0800

    Longest Increasing Subsequence

--- src/main/java/com/eden/explore/interviewclassic/intermediate/BacktrackingAlgorithm.java
+++ src/main/java/com/eden/explore/interviewclassic/intermediate/BacktrackingAlgorithm.java
@@ -273,6 +273,38 @@ public class BacktrackingAlgorithm {
         return dp[amount] == 999999 ? -1 : dp[amount];
     }
 
+
+    /**
+     * @Description: Longest Increasing Subsequence
+     * @Author: gexx
+     * @Date: 2020/6/4
+     **/
+    public int lengthOfLIS(int[] nums) {
+        int len = nums.length;
+        if (len == 0) {
+            return 0;
+        }
+        // 状态的定义是：以 i 结尾的最长上升子序列的长度
+        // 状态转移方程：之前比最后那个数字小的最长上升子序列的长度 + 1
+        int[] dp = new int[len];
+        Arrays.fill(dp, 1); // 如果只有 1 个元素，那么这个元素自己就构成了最长上升子序列，所以设置为 1 是合理的
+        for (int i = 1; i < len; i++) { // 从第 2 个元素开始，逐个写出 dp 数组的元素的值
+            int curVal = nums[i];
+            for (int j = 0; j < i; j++) { // 找出比当前元素小的哪些元素的最小值
+                if (curVal > nums[j]) {
+                    dp[i] = Integer.max(dp[j] + 1, dp[i]);
+                }
+            }
+        }
+        // 最后要全部走一遍，看最大值
+        int res = dp[0];
+        for (int i = 0; i < len; i++) {
+            res = Integer.max(res, dp[i]);
+        }
+        return res;
+    }
+
+
     public static void main(String[] args) {
         generateParenthesis(3);
         int[] b = {1, 2, 3};
