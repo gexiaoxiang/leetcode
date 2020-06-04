4c2b6c7697b7576855ed9caec647159a7fc8ad79
Apollo2016926 <944644308@qq.com>
2020-06-04 15:41:01 +0800

    零钱兑换

--- src/main/java/com/eden/explore/interviewclassic/intermediate/BacktrackingAlgorithm.java
+++ src/main/java/com/eden/explore/interviewclassic/intermediate/BacktrackingAlgorithm.java
@@ -256,6 +256,23 @@ public class BacktrackingAlgorithm {
         return arr[n - 1][m - 1];
     }
 
+    /**
+     * @Description: 零钱兑换
+     * @Author: gexx
+     * @Date: 2020/6/4
+     **/
+    public static int coinChange(int[] coins, int amount) {
+        if (amount == 0) return 0;
+        int[] dp = new int[amount + 1];  //dp[i]表示达到i用的最少硬币数  默认=初始化全为0
+        for (int i = 1; i <= amount; i++) {  //目标为0 的不用计算
+            dp[i] = 999999;   //此处不能用int的最大值，最大值+1 会溢出变为最小值
+            for (int coin : coins) {
+                if (i - coin >= 0) dp[i] = Math.min(dp[i - coin] + 1, dp[i]);
+            }
+        }
+        return dp[amount] == 999999 ? -1 : dp[amount];
+    }
+
     public static void main(String[] args) {
         generateParenthesis(3);
         int[] b = {1, 2, 3};
