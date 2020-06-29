c03509eb171d88231ca3d6f00d395aecdf3f644e
Apollo2016926 <944644308@qq.com>
2020-06-29 10:02:30 +0800

    最长连续序列

--- src/main/java/com/eden/explore/interviewclassic/intermediate/AdvancedAlgorithm.java
+++ src/main/java/com/eden/explore/interviewclassic/intermediate/AdvancedAlgorithm.java
@@ -1,5 +1,7 @@
 package com.eden.explore.interviewclassic.intermediate;
 
+import java.util.*;
+
 /**
  * @Description: Advanced algorithm
  * @Author gexx
@@ -103,8 +105,45 @@ public class AdvancedAlgorithm {
         return verify.length;
     }
 
-    public static void main(String[] args) {
-        int[] nums = {3, 4, -1, 1};
-        firstMissingPositive(nums);
+    /**
+     * @Description: 最长连续序列
+     * @Author: gexx
+     * @Date: 2020/6/28
+     **/
+    public static int longestConsecutive(int[] nums) {
+        Set<Integer> num_set = new HashSet<Integer>();
+        for (int num : nums) {
+            num_set.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for (int num : num_set) {
+            if (!num_set.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (num_set.contains(currentNum + 1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+
     }
+
+    public static void main(String[] args) {
+        Map map = new HashMap();
+        Object d = map.get("d");
+        int[] nums = {1, 0, -1};
+//        int[] nums = {1, 2, 0, 1};
+//        int[] nums = {4, 1, 3, 2};
+        longestConsecutive(nums);
+
+    }
+
 }
