29f15d2e75f597872cf9c30ba3fed0b46b208e89
Apollo2016926 <944644308@qq.com>
2020-06-29 17:21:01 +0800

    最大数

--- src/main/java/com/eden/explore/interviewclassic/intermediate/AdvancedAlgorithm.java
+++ src/main/java/com/eden/explore/interviewclassic/intermediate/AdvancedAlgorithm.java
@@ -461,6 +461,29 @@ public class AdvancedAlgorithm {
         return largestNumberStr;
     }
 
+
+    public static String multiply(String num1, String num2) {
+        if(num1.equals("0") || num2.equals("0")) return "0";
+        int m = num1.length();
+        int n = num2.length();
+        int[] mul = new int[m+n-1]; // 元素默认值为 0
+        for(int i = 0; i < m; ++i) { // 竖式运算
+            for(int j = 0; j < n; ++j) {
+                int vi = num1.charAt(i) - 48;
+                int vj = num2.charAt(j) - 48;
+                mul[i+j] += vi*vj;
+            }
+        }
+        for(int i = m+n-2; i > 0; --i) { // 进位
+            mul[i-1] += mul[i]/10;
+            mul[i] %= 10;
+        }
+        StringBuilder result = new StringBuilder();
+        for(int i = 0; i < m+n-1; ++i) { // 结果拼接为字符串
+            result.append(mul[i]);
+        }
+        return result.toString();
+    }
     public static void main(String[] args) {
         Map map = new HashMap();
         Object d = map.get("d");
@@ -477,6 +500,8 @@ public class AdvancedAlgorithm {
         wordBreak(s, Arrays.asList(wordDict));
         int[] sss = {3, 30, 34, 5, 9};
 
+        String  num1 = "123", num2 = "456";
+        multiply(num1,num2);
     }
 
 }
