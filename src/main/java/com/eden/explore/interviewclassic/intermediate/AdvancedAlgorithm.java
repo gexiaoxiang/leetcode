ced6d63976e63a6275b54e386b68b44c5e99865e
Apollo2016926 <944644308@qq.com>
2020-06-29 16:31:45 +0800

    最大数

--- src/main/java/com/eden/explore/interviewclassic/intermediate/AdvancedAlgorithm.java
+++ src/main/java/com/eden/explore/interviewclassic/intermediate/AdvancedAlgorithm.java
@@ -421,6 +421,45 @@ public class AdvancedAlgorithm {
 
     }
 
+    private class LargerNumberComparator implements Comparator<String> {
+        @Override
+        public int compare(String a, String b) {
+            String order1 = a + b;
+            String order2 = b + a;
+            return order2.compareTo(order1);
+        }
+    }
+
+    /**
+      * @Description: 最大数
+
+      * @Author: gexx
+      * @Date: 2020/6/29
+      **/
+    public String largestNumber(int[] nums) {
+        // Get input integers as strings.
+        String[] asStrs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            asStrs[i] = String.valueOf(nums[i]);
+        }
+
+        // Sort strings according to custom comparator.
+        Arrays.sort(asStrs, new LargerNumberComparator());
+
+        // If, after being sorted, the largest number is `0`, the entire number
+        // is zero.
+        if (asStrs[0].equals("0")) {
+            return "0";
+        }
+
+        // Build largest number from sorted array.
+        String largestNumberStr = new String();
+        for (String numAsStr : asStrs) {
+            largestNumberStr += numAsStr;
+        }
+
+        return largestNumberStr;
+    }
 
     public static void main(String[] args) {
         Map map = new HashMap();
@@ -436,6 +475,7 @@ public class AdvancedAlgorithm {
         String ss = "catsandog";
         String[] wordDict = {};
         wordBreak(s, Arrays.asList(wordDict));
+        int[] sss = {3, 30, 34, 5, 9};
 
     }
 
