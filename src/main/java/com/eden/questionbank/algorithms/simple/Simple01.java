9024721094b4cea56263f968705e045522294830
Apollo2016926 <944644308@qq.com>
2020-07-02 17:57:22 +0800

    二叉树的层次遍历 II

--- src/main/java/com/eden/questionbank/algorithms/simple/Simple01.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/Simple01.java
@@ -1,5 +1,10 @@
 package com.eden.questionbank.algorithms.simple;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /**
  * @Description: 简单
  * @Author gexx
@@ -70,6 +75,39 @@ public class Simple01 {
                 isSameTree(p.left, q.left);
 
     }
+
+    /**
+     * @Description: 二叉树的层次遍历 II
+     * @Author: gexx
+     * @Date: 2020/7/2
+     **/
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            List<Integer> tmp = new ArrayList<>();
+            int len = q.size();
+            for (int i = 0; i < len; i++) {
+                TreeNode node = q.poll();
+                tmp.add(node.val);
+                if (node.left != null) {
+                    q.add(node.left);
+                }
+                if (node.right != null) {
+                    q.add(node.right);
+                }
+            }
+            // 在索引 0 的位置加入一维数组 tmp
+            // 每次新的数组进来都会被放在开始的位置
+            ans.add(0, tmp);
+        }
+        return ans;
+    }
+
 
     public static void main(String[] args) {
         ListNode listNode1 = new ListNode(1);
