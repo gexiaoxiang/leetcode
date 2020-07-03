1d0eea1338f2336634b7f3a44b6c042ff0bf9470
Apollo2016926 <944644308@qq.com>
2020-07-03 11:15:49 +0800

    翻转二叉树

--- src/main/java/com/eden/questionbank/algorithms/simple/Simple01.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/Simple01.java
@@ -150,6 +150,23 @@ public class Simple01 {
         return sb.toString();
     }
 
+    /**
+     * @Description: 翻转二叉树
+     * @Author: gexx
+     * @Date: 2020/7/3
+     **/
+    public static TreeNode invertTree(TreeNode root) {
+
+        if (root == null) {
+            return null;
+        }
+        TreeNode right = invertTree(root.right);
+        TreeNode left = invertTree(root.left);
+        root.left = right;
+        root.right = left;
+        return root;
+
+    }
 
     public static void main(String[] args) {
         ListNode listNode1 = new ListNode(1);
@@ -174,6 +191,8 @@ public class Simple01 {
         root.right.right = r2;
         levelOrderBottom(root);
         minDepth(root);
+
+        invertTree(root);
 
     }
 }
