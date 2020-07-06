a9deaf07aa8c42c2fbd73b0d4f4580619c8499d0
Apollo2016926 <944644308@qq.com>
2020-07-06 14:59:17 +0800

    第三大的数

--- src/main/java/com/eden/questionbank/algorithms/simple/Simple02.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/Simple02.java
@@ -1,5 +1,9 @@
 package com.eden.questionbank.algorithms.simple;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @Description: 简单2  page2 size 100
  * @Author: gexx
@@ -27,7 +31,53 @@ public class Simple02 {
 
     }
 
+    /**
+     * @Description: 最长回文串
+     * @Author: gexx
+     * @Date: 2020/7/6
+     **/
+    public static int longestPalindrome(String s) {
+        int[] count = new int[128];
+        for (char c : s.toCharArray())
+            count[c]++;
+
+        int ans = 0;
+        for (int v : count) {
+            ans += v / 2 * 2;
+            if (v % 2 == 1 && ans % 2 == 0)
+                ans++;
+        }
+        return ans;
+    }
+
+/**
+  * @Description: 第三大的数
+
+  * @Author: gexx
+  * @Date: 2020/7/6
+  **/
+    public static int thirdMax(int[] nums) {
+        Arrays.sort(nums);
+        if (nums.length < 3) {
+            return nums[nums.length - 1];
+        }
+        List list = new ArrayList<>();
+        list.add(nums[nums.length - 1]);
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i + 1] != nums[i]) {
+                list.add(nums[i]);
+            }
+        }
+        if (list.size() < 3) {
+            return (int) list.get(0);
+        }
+
+        return (int) list.get(2);
+    }
+
     public static void main(String[] args) {
         System.out.println(toHex(26));
+        System.out.println(longestPalindrome("abccccdd"));
+        System.out.println(thirdMax(new int[]{1, 1, 2}));
     }
 }
