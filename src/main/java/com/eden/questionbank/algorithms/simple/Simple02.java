8b6ff80ec864fdf0cc398f180cd880f7a6b80da3
Apollo2016926 <944644308@qq.com>
2020-07-07 15:04:59 +0800

    岛屿的周长

--- src/main/java/com/eden/questionbank/algorithms/simple/Simple02.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/Simple02.java
@@ -243,6 +243,40 @@ public class Simple02 {
         return str.substring(1, str.length() - 1).contains(s);
     }
 
+    /**
+     * @Description: 岛屿的周长
+     * @Author: gexx
+     * @Date: 2020/7/7
+     **/
+    public static int islandPerimeter(int[][] grid) {
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[0].length; c++) {
+                if (grid[r][c] == 1) {
+                    // 题目限制只有一个岛屿，计算一个即可
+                    return dfs(grid, r, c);
+                }
+            }
+        }
+        return 0;
+    }
+
+    static int dfs(int[][] grid, int r, int c) {
+        if (!(0 <= r && r < grid.length && 0 <= c && c < grid[0].length)) {
+            return 1;
+        }
+        if (grid[r][c] == 0) {
+            return 1;
+        }
+        if (grid[r][c] != 1) {
+            return 0;
+        }
+        grid[r][c] = 2;
+        return dfs(grid, r - 1, c)
+                + dfs(grid, r + 1, c)
+                + dfs(grid, r, c - 1)
+                + dfs(grid, r, c + 1);
+    }
+
     public static void main(String[] args) {
         System.out.println(addStrings("1234", "1234"));
         System.out.println(toHex(26));
@@ -253,6 +287,12 @@ public class Simple02 {
         System.out.println(findDisappearedNumbers(new int[]{4, 3, 2, 7, 8, 2, 3, 1}));
         System.out.println(findContentChildren(new int[]{1, 2}, new int[]{1, 2, 3}));
         System.out.println(repeatedSubstringPattern("ababab"));
-
+        int[][] grid = {
+                {0, 1, 0, 0},
+                {1, 1, 1, 0},
+                {0, 1, 0, 0},
+                {1, 1, 0, 0}
+        };
+        System.out.println(islandPerimeter(grid));
     }
 }
