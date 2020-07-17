bead4a1eae1bcea1d5738af9f4633b959669ccf7
Apollo2016926 <944644308@qq.com>
2020-07-17 17:55:13 +0800

    根据二叉树创建字符串

--- src/main/java/com/eden/questionbank/algorithms/simple/Simple02.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/Simple02.java
@@ -851,6 +851,20 @@ public class Simple02 {
         return n <= 0;
     }
 
+    /**
+     * @Description: 根据二叉树创建字符串
+     * @Author: gexx
+     * @Date: 2020/7/17
+     **/
+    public String tree2str(TreeNode t) {
+        if (t == null)
+            return "";
+        if (t.left == null && t.right == null)
+            return t.val + "";
+        if (t.right == null)
+            return t.val + "(" + tree2str(t.left) + ")";
+        return t.val + "(" + tree2str(t.left) + ")(" + tree2str(t.right) + ")";
+    }
 
 
     public static void main(String[] args) {
