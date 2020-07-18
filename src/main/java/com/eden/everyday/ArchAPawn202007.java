91a05cd7d2fe5a40dad715a7b0b2620b76e4c636
gexx <944644308@qq.com>
2020-07-18 15:35:17 +0800

    交错字符串

--- src/main/java/com/eden/everyday/ArchAPawn202007.java
+++ src/main/java/com/eden/everyday/ArchAPawn202007.java
@@ -23,4 +23,35 @@ public class ArchAPawn202007 {
 
         return 1;
     }
+
+    /**
+     * @Description 交错字符串
+     *
+     * @Author gexx
+     * @Date 2020/7/18
+     **/
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int n = s1.length(), m = s2.length(), t = s3.length();
+
+        if (n + m != t) {
+            return false;
+        }
+
+        boolean[] f = new boolean[m + 1];
+
+        f[0] = true;
+        for (int i = 0; i <= n; ++i) {
+            for (int j = 0; j <= m; ++j) {
+                int p = i + j - 1;
+                if (i > 0) {
+                    f[j] = f[j] && s1.charAt(i - 1) == s3.charAt(p);
+                }
+                if (j > 0) {
+                    f[j] = f[j] || (f[j - 1] && s2.charAt(j - 1) == s3.charAt(p));
+                }
+            }
+        }
+
+        return f[m];
+    }
 }
