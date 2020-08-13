a03f2b7611122a9538ffc7c95b8270b4e455eb01
Apollo2016926 <944644308@qq.com>
2020-08-13 18:00:40 +0800

    字符串相乘

--- src/main/java/com/eden/everyday/ArchAPawn202008.java
+++ src/main/java/com/eden/everyday/ArchAPawn202008.java
@@ -7,16 +7,14 @@ import java.util.PriorityQueue;
 
 
 /**
- * @Description 每日一题
- *
  * @author gexx
+ * @Description 每日一题
  * @Date 2020/8/1
  **/
 public class ArchAPawn202008 {
 
     /**
      * @Description最小区间
-     *
      * @Author gexx
      * @Date 2020/8/1
      **/
@@ -54,24 +52,29 @@ public class ArchAPawn202008 {
     }
 
     public class TreeNode {
-       int val;
-       TreeNode left;
-       TreeNode right;
-       TreeNode() {}
-       TreeNode(int val) { this.val = val; }
-       TreeNode(int val, TreeNode left, TreeNode right) {
-           this.val = val;
-           this.left = left;
-           this.right = right;
-       }
-   }
+        int val;
+        TreeNode left;
+        TreeNode right;
 
-   /**
-    * @Description二叉树展开为链表
-    *
-    * @author gexx
-    * @Date 2020/8/2
-    **/
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /**
+     * @Description二叉树展开为链表
+     * @author gexx
+     * @Date 2020/8/2
+     **/
     public void flatten(TreeNode root) {
         List<TreeNode> list = new ArrayList<TreeNode>();
         preorderTraversal(root, list);
@@ -91,4 +94,93 @@ public class ArchAPawn202008 {
         }
     }
 
+    List<List<Integer>> edges;
+    int[] visited;
+    boolean valid = true;
+
+    /**
+     * @Description: 课程表
+     * @Author: gexx
+     * @Date: 2020/8/4
+     **/
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        edges = new ArrayList<List<Integer>>();
+        for (int i = 0; i < numCourses; ++i) {
+            edges.add(new ArrayList<Integer>());
+        }
+        visited = new int[numCourses];
+        for (int[] info : prerequisites) {
+            edges.get(info[1]).add(info[0]);
+        }
+        for (int i = 0; i < numCourses && valid; ++i) {
+            if (visited[i] == 0) {
+                dfs(i);
+            }
+        }
+        return valid;
+    }
+
+    public void dfs(int u) {
+        visited[u] = 1;
+        for (int v : edges.get(u)) {
+            if (visited[v] == 0) {
+                dfs(v);
+                if (!valid) {
+                    return;
+                }
+            } else if (visited[v] == 1) {
+                valid = false;
+                return;
+            }
+        }
+        visited[u] = 2;
+    }
+
+    /**
+     * @Description: 字符串相乘
+     * @Author: gexx
+     * @Date: 2020/8/13
+     **/
+    public String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+        String ans = "0";
+        int m = num1.length(), n = num2.length();
+        for (int i = n - 1; i >= 0; i--) {
+            StringBuffer curr = new StringBuffer();
+            int add = 0;
+            for (int j = n - 1; j > i; j--) {
+                curr.append(0);
+            }
+            int y = num2.charAt(i) - '0';
+            for (int j = m - 1; j >= 0; j--) {
+                int x = num1.charAt(j) - '0';
+                int product = x * y + add;
+                curr.append(product % 10);
+                add = product / 10;
+            }
+            if (add != 0) {
+                curr.append(add % 10);
+            }
+            ans = addStrings(ans, curr.reverse().toString());
+        }
+        return ans;
+    }
+
+    public String addStrings(String num1, String num2) {
+        int i = num1.length() - 1, j = num2.length() - 1, add = 0;
+        StringBuffer ans = new StringBuffer();
+        while (i >= 0 || j >= 0 || add != 0) {
+            int x = i >= 0 ? num1.charAt(i) - '0' : 0;
+            int y = j >= 0 ? num2.charAt(j) - '0' : 0;
+            int result = x + y + add;
+            ans.append(result % 10);
+            add = result / 10;
+            i--;
+            j--;
+        }
+        ans.reverse();
+        return ans.toString();
+    }
 }
