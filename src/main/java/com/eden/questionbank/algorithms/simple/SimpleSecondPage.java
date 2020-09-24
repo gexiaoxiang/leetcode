836f1666a01a7a422674c3b85b25a26df02fa0ea
Apollo2016926 <944644308@qq.com>
2020-09-24 10:38:34 +0800

    词典中最长的单词

--- src/main/java/com/eden/questionbank/algorithms/simple/SimpleSecondPage.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/SimpleSecondPage.java
@@ -1286,6 +1286,31 @@ public class SimpleSecondPage {
         return false;
     }
 
+    /**
+     * @Description: 词典中最长的单词
+     * @Author: gexx
+     * @Date: 2020/9/24
+     **/
+    public String longestWord(String[] words) {
+        String ans = "";
+        Set<String> wordset = new HashSet();
+        for (String word : words) wordset.add(word);
+        for (String word : words) {
+            if (word.length() > ans.length() ||
+                    word.length() == ans.length() && word.compareTo(ans) < 0) {
+                boolean good = true;
+                for (int k = 1; k < word.length(); ++k) {
+                    if (!wordset.contains(word.substring(0, k))) {
+                        good = false;
+                        break;
+                    }
+                }
+                if (good) ans = word;
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         toLowerCase("Hello");
     }
