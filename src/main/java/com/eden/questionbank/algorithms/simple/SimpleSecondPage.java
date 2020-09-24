789dc262fe34d69e8328bab2e3270ce8d2efc2b0
Apollo2016926 <944644308@qq.com>
2020-09-24 10:41:51 +0800

    自除数

--- src/main/java/com/eden/questionbank/algorithms/simple/SimpleSecondPage.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/SimpleSecondPage.java
@@ -1311,6 +1311,27 @@ public class SimpleSecondPage {
         return ans;
     }
 
+    /**
+     * 自除数      * @Author: gexx
+     *
+     * @Date: 2020/9/24
+     **/
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> ans = new ArrayList();
+        for (int n = left; n <= right; ++n) {
+            if (selfDividing(n)) ans.add(n);
+        }
+        return ans;
+    }
+
+    public boolean selfDividing(int n) {
+        for (char c : String.valueOf(n).toCharArray()) {
+            if (c == '0' || (n % (c - '0') > 0))
+                return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         toLowerCase("Hello");
     }
