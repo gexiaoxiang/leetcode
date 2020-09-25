9d435c81595b399f64a53a6873aafc7fa970c4d5
Apollo2016926 <944644308@qq.com>
2020-09-25 11:24:57 +0800

    二进制表示中质数个计算置位

--- src/main/java/com/eden/questionbank/algorithms/simple/SimpleSecondPage.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/SimpleSecondPage.java
@@ -1379,6 +1379,24 @@ public class SimpleSecondPage {
         }
         return ans;
     }
+/**
+  * @Description: 二进制表示中质数个计算置位
+
+  * @Author: gexx
+  * @Date: 2020/9/25
+  **/
+    public int countPrimeSetBits(int L, int R) {
+        int ans = 0;
+        for (int x = L; x <= R; ++x)
+            if (isSmallPrime(Integer.bitCount(x)))
+                ans++;
+        return ans;
+    }
+
+    public boolean isSmallPrime(int x) {
+        return (x == 2 || x == 3 || x == 5 || x == 7 ||
+                x == 11 || x == 13 || x == 17 || x == 19);
+    }
 
     public static void main(String[] args) {
         minCostClimbingStairs(new int[]{1, 100, 1, 1, 1, 100, 1, 1, 100, 1});
