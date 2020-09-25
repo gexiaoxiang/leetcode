75d44cec0db85a720d856ae663a407b72d942e86
Apollo2016926 <944644308@qq.com>
2020-09-25 11:16:22 +0800

    最短完整词

--- src/main/java/com/eden/questionbank/algorithms/simple/SimpleSecondPage.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/SimpleSecondPage.java
@@ -1337,7 +1337,7 @@ public class SimpleSecondPage {
      * @Author: gexx
      * @Date: 2020/9/25
      **/
-    public int minCostClimbingStairs(int[] cost) {
+    public static int minCostClimbingStairs(int[] cost) {
         int f1 = 0, f2 = 0;
         for (int i = cost.length - 1; i >= 0; --i) {
             int f0 = cost[i] + Math.min(f1, f2);
@@ -1348,7 +1348,40 @@ public class SimpleSecondPage {
 
     }
 
+    /**
+     * @Description: 最短完整词
+     * @Author: gexx
+     * @Date: 2020/9/25
+     **/
+    public String shortestCompletingWord(String licensePlate, String[] words) {
+        int[] target = count(licensePlate);
+        String ans = "";
+        for (String word : words)
+            if ((word.length() < ans.length() || ans.length() == 0) &&
+                    dominates(count(word.toLowerCase()), target))
+                ans = word;
+        return ans;
+    }
+
+    public boolean dominates(int[] count1, int[] count2) {
+        for (int i = 0; i < 26; ++i)
+            if (count1[i] < count2[i])
+                return false;
+        return true;
+    }
+
+    public int[] count(String word) {
+        int[] ans = new int[26];
+        for (char letter : word.toCharArray()) {
+            int index = Character.toLowerCase(letter) - 'a';
+            if (0 <= index && index < 26)
+                ans[index]++;
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
+        minCostClimbingStairs(new int[]{1, 100, 1, 1, 1, 100, 1, 1, 100, 1});
         toLowerCase("Hello");
     }
 }
