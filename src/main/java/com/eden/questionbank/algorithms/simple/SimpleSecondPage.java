6eb2a1786f105f45f0391bc22083cc90dfad0d0a
Apollo2016926 <944644308@qq.com>
2020-09-27 10:27:05 +0800

    旋转数字

--- src/main/java/com/eden/questionbank/algorithms/simple/SimpleSecondPage.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/SimpleSecondPage.java
@@ -1418,6 +1418,27 @@ public class SimpleSecondPage {
 
     }
 
+    /**
+     * @Description: 旋转数字
+     * @Author: gexx
+     * @Date: 2020/9/27
+     **/
+    public int rotatedDigits(int N) {
+        int ans = 0;
+        for (int n = 1; n <= N; ++n)
+            if (good(n, false)) ans++;
+        return ans;
+    }
+
+    public boolean good(int n, boolean flag) {
+        if (n == 0) return flag;
+
+        int d = n % 10;
+        if (d == 3 || d == 4 || d == 7) return false;
+        if (d == 0 || d == 1 || d == 8) return good(n / 10, flag);
+        return good(n / 10, true);
+    }
+
     public static void main(String[] args) {
         minCostClimbingStairs(new int[]{1, 100, 1, 1, 1, 100, 1, 1, 100, 1});
         toLowerCase("Hello");
