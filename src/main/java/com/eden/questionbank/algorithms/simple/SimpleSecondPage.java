9d5727db162f1d72f65b50e1f778a9736a9cbe71
Apollo2016926 <944644308@qq.com>
2020-12-10 16:16:53 +0800

    最常见的单词

--- src/main/java/com/eden/questionbank/algorithms/simple/SimpleSecondPage.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/SimpleSecondPage.java
@@ -1572,13 +1572,51 @@ public class SimpleSecondPage {
         }
         return 0;
     }
+
     //计算2点之间的距离
-    public double distance(int[] a, int[] b){
-        double x=b[0]-a[0];
-        double y=b[1]-a[1];
-        return  Math.sqrt(Math.pow(x,2)+Math.pow(y,2));
+    public double distance(int[] a, int[] b) {
+        double x = b[0] - a[0];
+        double y = b[1] - a[1];
+        return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
     }
+
+    /**
+     * @Description: 819. 最常见的单词
+     * @Author: gexx
+     * @Date: 2020/12/10
+     **/
+    public static String mostCommonWord(String paragraph, String[] banned) {
+        paragraph += ".";
+
+        Set<String> banset = new HashSet();
+        for (String word: banned) banset.add(word);
+        Map<String, Integer> count = new HashMap();
+
+        String ans = "";
+        int ansfreq = 0;
+
+        StringBuilder word = new StringBuilder();
+        for (char c: paragraph.toCharArray()) {
+            if (Character.isLetter(c)) {
+                word.append(Character.toLowerCase(c));
+            } else if (word.length() > 0) {
+                String finalword = word.toString();
+                if (!banset.contains(finalword)) {
+                    count.put(finalword, count.getOrDefault(finalword, 0) + 1);
+                    if (count.get(finalword) > ansfreq) {
+                        ans = finalword;
+                        ansfreq = count.get(finalword);
+                    }
+                }
+                word = new StringBuilder();
+            }
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
+        mostCommonWord("a, a, a, a, b,b,b,c, c", new String[]{"a"});
         subdomainVisits(new String[]{"9001 discuss.leetcode.com"});
         minCostClimbingStairs(new int[]{1, 100, 1, 1, 1, 100, 1, 1, 100, 1});
         toLowerCase("Hello");
