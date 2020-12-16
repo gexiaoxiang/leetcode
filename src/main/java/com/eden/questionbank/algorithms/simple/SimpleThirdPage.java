48e46d5fc90908ae264f143843bcc89935aeec00
Apollo2016926 <944644308@qq.com>
2020-12-16 18:30:09 +0800

    亲密字符串

--- src/main/java/com/eden/questionbank/algorithms/simple/SimpleThirdPage.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/SimpleThirdPage.java
@@ -41,23 +41,58 @@ public class SimpleThirdPage {
      * @Author: gexx
      * @Date: 2020/12/16
      **/
-    public  static  int peakIndexInMountainArray(int[] arr) {
-        int top=-1;
-        int topIndex=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>top){
-            top=arr[i];
-            topIndex=i;
-            }else{
+    public static int peakIndexInMountainArray(int[] arr) {
+        int top = -1;
+        int topIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > top) {
+                top = arr[i];
+                topIndex = i;
+            } else {
                 return topIndex;
             }
 
 
-    }
+        }
         return topIndex;
     }
 
+    /**
+     * @Description: 859. 亲密字符串
+     * @Author: gexx
+     * @Date: 2020/12/16
+     **/
+    public static boolean buddyStrings(String A, String B) {
+        if (A.length() != B.length()) return false;
+        if (A.equals(B)) {
+            int[] count = new int[26];
+            for (int i = 0; i < A.length(); ++i)
+                count[A.charAt(i) - 'a']++;
+
+            for (int c : count)
+                if (c > 1) return true;
+            return false;
+        } else {
+            int first = -1, second = -1;
+            for (int i = 0; i < A.length(); ++i) {
+                if (A.charAt(i) != B.charAt(i)) {
+                    if (first == -1)
+                        first = i;
+                    else if (second == -1)
+                        second = i;
+                    else
+                        return false;
+                }
+            }
+
+            return (second != -1 && A.charAt(first) == B.charAt(second) &&
+                    A.charAt(second) == B.charAt(first));
+        }
+    }
+
+
     public static void main(String[] args) {
-peakIndexInMountainArray(new int[]{0,1,0});
+        peakIndexInMountainArray(new int[]{0, 1, 0});
+        buddyStrings("ab", "ba");
     }
 }
