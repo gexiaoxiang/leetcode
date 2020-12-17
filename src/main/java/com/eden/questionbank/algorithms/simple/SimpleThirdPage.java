5d8447d338253bb817840bd875be469497860eaa
Apollo2016926 <944644308@qq.com>
2020-12-17 15:50:37 +0800

    叶子相似的树

--- src/main/java/com/eden/questionbank/algorithms/simple/SimpleThirdPage.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/SimpleThirdPage.java
@@ -141,6 +141,56 @@ public class SimpleThirdPage {
 
     }
 
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /**
+     * @Description: 872. 叶子相似的树
+     * @Author: gexx
+     * @Date: 2020/12/17
+     **/
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+
+        List<Integer> list1 = new ArrayList();
+        List<Integer> list2 = new ArrayList();
+
+
+        dfs(root1,list1);
+        dfs(root2,list2);
+
+        return list1.equals(list2);
+
+
+    }
+
+    private void dfs(TreeNode node, List<Integer> list) {
+        if(node!=null){
+            if(node.left==null&&node.right==null){
+                list.add(node.val);
+            }
+            dfs(node.left,list);
+            dfs(node.right,list);
+
+        }
+    }
+
     public static void main(String[] args) {
         binaryGap(22);
         peakIndexInMountainArray(new int[]{0, 1, 0});
