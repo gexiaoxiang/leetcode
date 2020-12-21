b3d5f397b045e7cc153db78c4ae9c0e06dcecf4e
Apollo2016926 <944644308@qq.com>
2020-12-21 14:25:12 +0800

    三维形体的表面积

--- src/main/java/com/eden/questionbank/algorithms/simple/SimpleThirdPage.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/SimpleThirdPage.java
@@ -349,6 +349,40 @@ public class SimpleThirdPage {
 
     }
 
+    /**
+     * @Description: 892. 三维形体的表面积
+     * @Author: gexx
+     * @Date: 2020/12/21
+     **/
+    public int surfaceArea(int[][] grid) {
+        int[] dr = new int[]{0, 1, 0, -1};
+        int[] dc = new int[]{1, 0, -1, 0};
+
+        int N = grid.length;
+        int ans = 0;
+
+        for (int r = 0; r < N; ++r) {
+            for (int c = 0; c < N; ++c) {
+                if (grid[r][c] > 0) {
+                    ans += 2;
+                    for (int k = 0; k < 4; ++k) {
+                        int nr = r + dr[k];
+                        int nc = c + dc[k];
+                        int nv = 0;
+                        if (0 <= nr && nr < N && 0 <= nc && nc < N) {
+                            nv = grid[nr][nc];
+                        }
+
+                        ans += Math.max(grid[r][c] - nv, 0);
+                    }
+                }
+            }
+        }
+
+        return ans;
+
+    }
+
     public static void main(String[] args) {
         uncommonFromSentences("d b zu d t",
                 "udb zu ap");
