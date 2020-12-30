b9a6cddd15abf18dc300680e96e0d8176fbef8c7
Apollo2016926 <944644308@qq.com>
2020-12-30 16:05:02 +0800

    卡牌分组

--- src/main/java/com/eden/questionbank/algorithms/simple/SimpleThirdPage.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/SimpleThirdPage.java
@@ -481,8 +481,44 @@ public class SimpleThirdPage {
 
     }
 
-    public static void main(String[] args) {
+    /**
+     * @Description: 914. 卡牌分组
+     * @Author: gexx
+     * @Date: 2020/12/28
+     **/
+    public static boolean hasGroupsSizeX(int[] deck) {
+        int N = deck.length;
+        int[] count = new int[10000];
+        for (int c : deck) {
+            count[c]++;
+        }
 
+        List<Integer> values = new ArrayList<Integer>();
+        for (int i = 0; i < 10000; ++i) {
+            if (count[i] > 0) {
+                values.add(count[i]);
+            }
+        }
+
+        for (int X = 2; X <= N; ++X) {
+            if (N % X == 0) {
+                boolean flag = true;
+                for (int v : values) {
+                    if (v % X != 0) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        hasGroupsSizeX(new int[]{1, 2, 3, 4, 4, 3, 2, 1});
         sortArrayByParity(new int[]{
                 3, 1, 2, 4});
         uncommonFromSentences("d b zu d t",
