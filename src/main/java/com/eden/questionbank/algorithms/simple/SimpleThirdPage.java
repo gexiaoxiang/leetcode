c9d9c750a4c5671bec207c5c861e16862970e5e1
Apollo2016926 <944644308@qq.com>
2020-12-30 17:42:33 +0800

    长按键入

--- src/main/java/com/eden/questionbank/algorithms/simple/SimpleThirdPage.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/SimpleThirdPage.java
@@ -568,6 +568,27 @@ public class SimpleThirdPage {
         return AC;
     }
 
+    /**
+     * @Description: 925. 长按键入
+     * @Author: gexx
+     * @Date: 2020/12/30
+     **/
+    public boolean isLongPressedName(String name, String typed) {
+        int i = 0, j = 0;
+        while (j < typed.length()) {
+            if (i < name.length() && name.charAt(i) == typed.charAt(j)) {
+                i++;
+                j++;
+            } else if (j > 0 && typed.charAt(j) == typed.charAt(j - 1)) {
+                j++;
+            } else {
+                return false;
+            }
+        }
+        return i == name.length();
+    }
+
+
     public static void main(String[] args) {
         reverseOnlyLetters("a-bC-dEf-ghIj"
         );
