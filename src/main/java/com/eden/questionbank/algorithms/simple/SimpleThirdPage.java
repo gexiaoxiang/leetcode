0c4572f4e025c4b1ca701848b7882e17f573c7d2
Apollo2016926 <944644308@qq.com>
2021-01-06 14:29:16 +0800

    删列造序

--- src/main/java/com/eden/questionbank/algorithms/simple/SimpleThirdPage.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/SimpleThirdPage.java
@@ -719,6 +719,24 @@ public class SimpleThirdPage {
         return ans;
     }
 
+    /**
+     * @Description: 944. 删列造序
+     * @Author: gexx
+     * @Date: 2021/1/6
+     **/
+    public int minDeletionSize(String[] A) {
+        int ans = 0;
+        for (int c = 0; c < A[0].length(); ++c)
+            for (int r = 0; r < A.length - 1; ++r)
+                if (A[r].charAt(c) > A[r + 1].charAt(c)) {
+                    ans++;
+                    break;
+                }
+
+        return ans;
+    }
+
+
     public static void main(String[] args) {
         diStringMatch("IDID");
 
