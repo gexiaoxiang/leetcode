3263cde91d6c61dabb799f2be023e4407e4dc915
Apollo2016926 <944644308@qq.com>
2021-01-06 14:36:06 +0800

    验证外星语词典

--- src/main/java/com/eden/questionbank/algorithms/simple/SimpleThirdPage.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/SimpleThirdPage.java
@@ -736,6 +736,36 @@ public class SimpleThirdPage {
         return ans;
     }
 
+    /**
+     * @Description: 953. 验证外星语词典
+     * @Author: gexx
+     * @Date: 2021/1/6
+     **/
+    public boolean isAlienSorted(String[] words, String order) {
+        int[] index = new int[26];
+        for (int i = 0; i < order.length(); ++i)
+            index[order.charAt(i) - 'a'] = i;
+
+        search:
+        for (int i = 0; i < words.length - 1; ++i) {
+            String word1 = words[i];
+            String word2 = words[i + 1];
+
+            for (int k = 0; k < Math.min(word1.length(), word2.length()); ++k) {
+                if (word1.charAt(k) != word2.charAt(k)) {
+                    if (index[word1.charAt(k) - 'a'] > index[word2.charAt(k) - 'a'])
+                        return false;
+                    continue search;
+                }
+            }
+
+            if (word1.length() > word2.length())
+                return false;
+        }
+
+        return true;
+
+    }
 
     public static void main(String[] args) {
         diStringMatch("IDID");
