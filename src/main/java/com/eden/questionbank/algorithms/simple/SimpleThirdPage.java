e20d3a988734914213438756118203f065456492
Apollo2016926 <944644308@qq.com>
2021-01-07 10:01:30 +0800

    强整数

--- src/main/java/com/eden/questionbank/algorithms/simple/SimpleThirdPage.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/SimpleThirdPage.java
@@ -803,6 +803,23 @@ public class SimpleThirdPage {
 
     }
 
+    /**
+     * @Description: 970. 强整数
+     * @Author: gexx
+     * @Date: 2021/1/7
+     **/
+    public List<Integer> powerfulIntegers(int x, int y, int bound) {
+
+        Set<Integer> seen = new HashSet();
+        for (int i = 0; i < 18 && Math.pow(x, i) <= bound; ++i)
+            for (int j = 0; j < 18 && Math.pow(y, j) <= bound; ++j) {
+                int v = (int) Math.pow(x, i) + (int) Math.pow(y, j);
+                if (v <= bound)
+                    seen.add(v);
+            }
+
+        return new ArrayList(seen);
+    }
 
     public static void main(String[] args) {
         repeatedNTimes(new int[]{5, 1, 5, 2, 5, 3, 5, 4});
