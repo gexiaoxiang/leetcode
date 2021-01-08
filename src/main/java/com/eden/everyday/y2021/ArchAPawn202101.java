33ac5f65f1a167fedbd4c88e9f920f2645ac2201
Apollo2016926 <944644308@qq.com>
2021-01-08 17:01:52 +0800

    数组形式的整数加法

--- src/main/java/com/eden/everyday/y2021/ArchAPawn202101.java
+++ src/main/java/com/eden/everyday/y2021/ArchAPawn202101.java
@@ -22,9 +22,26 @@ public class ArchAPawn202101 {
     }
 
 
+    /**
+     * @Description: 189. 旋转数组
+     * @Author: gexx
+     * @Date: 2021/1/8
+     **/
+    public static void rotate(int[] nums, int k) {
+        int n = nums.length;
+        int[] newArr = new int[n];
+        for (int i = 0; i < n; ++i) {
+            newArr[(i + k) % n] = nums[i];
+        }
+        System.arraycopy(newArr, 0, nums, 0, n);
 
+
+    }
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString("12.22".split("\\|")));
+        rotate(new int[]{
+                1, 2, 3, 4, 5, 6, 7
+        }, 3);
     }
 }
