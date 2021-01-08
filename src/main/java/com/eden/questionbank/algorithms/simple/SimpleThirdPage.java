e64d5dd3cea590a4cb4ba292c13d82f5a59b0163
Apollo2016926 <944644308@qq.com>
2021-01-08 11:07:39 +0800

    查询后的偶数和

--- src/main/java/com/eden/questionbank/algorithms/simple/SimpleThirdPage.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/SimpleThirdPage.java
@@ -855,6 +855,32 @@ public class SimpleThirdPage {
         Arrays.sort(nums);
         return nums;
     }
+
+    /**
+     * @Description: 985. 查询后的偶数和
+     * @Author: gexx
+     * @Date: 2021/1/8
+     **/
+    public int[] sumEvenAfterQueries(int[] A, int[][] queries) {
+        int S = 0;
+        for (int x : A)
+            if (x % 2 == 0)
+                S += x;
+
+        int[] ans = new int[queries.length];
+
+        for (int i = 0; i < queries.length; ++i) {
+            int val = queries[i][0], index = queries[i][1];
+            if (A[index] % 2 == 0) S -= A[index];
+            A[index] += val;
+            if (A[index] % 2 == 0) S += A[index];
+            ans[i] = S;
+        }
+
+        return ans;
+
+    }
+
     public static void main(String[] args) {
         repeatedNTimes(new int[]{5, 1, 5, 2, 5, 3, 5, 4});
         diStringMatch("IDID");
