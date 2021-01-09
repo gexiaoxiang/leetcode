776fd72c2ea5380fb0e8bcaefae950ed614f1977
Apollo2016926 <944644308@qq.com>
2021-01-09 12:17:57 +0800

    可以被一步捕获的棋子数

--- src/main/java/com/eden/questionbank/algorithms/simple/SimpleThirdPage.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/SimpleThirdPage.java
@@ -955,6 +955,41 @@ public class SimpleThirdPage {
     }
 
 
+    /**
+     * @Description 999. 可以被一步捕获的棋子数
+     * @Author gexx
+     * @Date 2021/1/9
+     **/
+    public int numRookCaptures(char[][] board) {
+        int cnt = 0, st = 0, ed = 0;
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
+        for (int i = 0; i < 8; ++i) {
+            for (int j = 0; j < 8; ++j) {
+                if (board[i][j] == 'R') {
+                    st = i;
+                    ed = j;
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < 4; ++i) {
+            for (int step = 0; ; ++step) {
+                int tx = st + step * dx[i];
+                int ty = ed + step * dy[i];
+                if (tx < 0 || tx >= 8 || ty < 0 || ty >= 8 || board[tx][ty] == 'B') {
+                    break;
+                }
+                if (board[tx][ty] == 'p') {
+                    cnt++;
+                    break;
+                }
+            }
+        }
+        return cnt;
+
+    }
+
     public static void main(String[] args) {
         findJudge(2, new int[][]{{1, 2}});
         addToArrayForm(new int[]{9, 9, 9, 9, 9, 9, 9, 9, 9, 9}
