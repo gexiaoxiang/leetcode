f4219f6ca01e5ec8a8412a7c46872a78966c303c
Apollo2016926 <944644308@qq.com>
2021-01-09 12:25:15 +0800

    买卖股票的最佳时机

--- src/main/java/com/eden/everyday/y2021/ArchAPawn202101.java
+++ src/main/java/com/eden/everyday/y2021/ArchAPawn202101.java
@@ -38,7 +38,27 @@ public class ArchAPawn202101 {
 
     }
 
+    /**
+     * @Description 123. 买卖股票的最佳时机 III
+     * @Author gexx
+     * @Date 2021/1/9
+     **/
+    public static int maxProfit(int[] prices) {
+        int n = prices.length;
+        int buy1 = -prices[0], sell1 = 0;
+        int buy2 = -prices[0], sell2 = 0;
+        for (int i = 1; i < n; ++i) {
+            buy1 = Math.max(buy1, -prices[i]);
+            sell1 = Math.max(sell1, buy1 + prices[i]);
+            buy2 = Math.max(buy2, sell1 - prices[i]);
+            sell2 = Math.max(sell2, buy2 + prices[i]);
+        }
+        return sell2;
+
+    }
+
     public static void main(String[] args) {
+        maxProfit(new int[]{3, 3, 5, 0, 0, 3, 1, 4});
         System.out.println(Arrays.toString("12.22".split("\\|")));
         rotate(new int[]{
                 1, 2, 3, 4, 5, 6, 7
