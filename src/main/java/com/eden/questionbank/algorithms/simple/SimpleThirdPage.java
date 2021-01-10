f9676c66a32a0767c6d18495aab9bfb505b50fdd
Apollo2016926 <944644308@qq.com>
2021-01-10 14:00:17 +0800

    查找常用字符

--- src/main/java/com/eden/questionbank/algorithms/simple/SimpleThirdPage.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/SimpleThirdPage.java
@@ -990,6 +990,36 @@ public class SimpleThirdPage {
 
     }
 
+    /**
+     * @Description 1002. 查找常用字符
+     * @author gexx
+     * @Date 2021/1/10
+     **/
+    public List<String> commonChars(String[] A) {
+        int[] minfreq = new int[26];
+        Arrays.fill(minfreq, Integer.MAX_VALUE);
+        for (String word: A) {
+            int[] freq = new int[26];
+            int length = word.length();
+            for (int i = 0; i < length; ++i) {
+                char ch = word.charAt(i);
+                ++freq[ch - 'a'];
+            }
+            for (int i = 0; i < 26; ++i) {
+                minfreq[i] = Math.min(minfreq[i], freq[i]);
+            }
+        }
+
+        List<String> ans = new ArrayList<String>();
+        for (int i = 0; i < 26; ++i) {
+            for (int j = 0; j < minfreq[i]; ++j) {
+                ans.add(String.valueOf((char) (i + 'a')));
+            }
+        }
+        return ans;
+
+    }
+
     public static void main(String[] args) {
         findJudge(2, new int[][]{{1, 2}});
         addToArrayForm(new int[]{9, 9, 9, 9, 9, 9, 9, 9, 9, 9}
