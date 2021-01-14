ca276d501c2083f35db0b59c4f4dffe094ff89bc
Apollo2016926 <944644308@qq.com>
2021-01-14 18:52:21 +0800

    字符串的最大公因子

--- src/main/java/com/eden/questionbank/algorithms/simple/SimpleThirdPage.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/SimpleThirdPage.java
@@ -1299,6 +1299,34 @@ public class SimpleThirdPage {
     }
 
 
+    /**
+     * @Description: 1071. 字符串的最大公因子
+     * @Author: gexx
+     * @Date: 2021/1/14
+     **/
+    public String gcdOfStrings(String str1, String str2) {
+
+        int len1 = str1.length(), len2 = str2.length();
+        for (int i = Math.min(len1, len2); i >= 1; --i) { // 从长度大的开始枚举
+            if (len1 % i == 0 && len2 % i == 0) {
+                String X = str1.substring(0, i);
+                if (check(X, str1) && check(X, str2)) {
+                    return X;
+                }
+            }
+        }
+        return "";
+    }
+
+    public boolean check(String t, String s) {
+        int lenx = s.length() / t.length();
+        StringBuffer ans = new StringBuffer();
+        for (int i = 1; i <= lenx; ++i) {
+            ans.append(t);
+        }
+        return ans.toString().equals(s);
+    }
+
     public static void main(String[] args) {
         removeDuplicates("abbaca");
 
