5e6ede16268d49ad55ab2b2ccfb3f028ae562e64
Apollo2016926 <944644308@qq.com>
2021-02-05 15:08:35 +0800

    尽可能使字符串相等

--- src/main/java/com/eden/everyday/y2021/ArchAPawn202102.java
+++ src/main/java/com/eden/everyday/y2021/ArchAPawn202102.java
@@ -152,6 +152,32 @@ public class ArchAPawn202102 {
 
     }
 
+    /**
+     * @Description: 1208. 尽可能使字符串相等
+     * @Author: gexx
+     * @Date: 2021/2/5
+     **/
+    public int equalSubstring(String s, String t, int maxCost) {
+        int n = s.length();
+        int[] diff = new int[n];
+        for (int i = 0; i < n; i++) {
+            diff[i] = Math.abs(s.charAt(i) - t.charAt(i));
+        }
+        int maxLength = 0;
+        int start = 0, end = 0;
+        int sum = 0;
+        while (end < n) {
+            sum += diff[end];
+            while (sum > maxCost) {
+                sum -= diff[start];
+                start++;
+            }
+            maxLength = Math.max(maxLength, end - start + 1);
+            end++;
+        }
+        return maxLength;
+    }
+
     public static void main(String[] args) {
 
         characterReplacement("AABBBCBB", 1);
