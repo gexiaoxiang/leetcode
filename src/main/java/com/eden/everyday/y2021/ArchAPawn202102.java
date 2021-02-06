82d3d9d33728d9e9bbd61901622a319552fd19b9
Apollo2016926 <944644308@qq.com>
2021-02-06 13:45:11 +0800

    可获得的最大点数

--- src/main/java/com/eden/everyday/y2021/ArchAPawn202102.java
+++ src/main/java/com/eden/everyday/y2021/ArchAPawn202102.java
@@ -1,9 +1,6 @@
 package com.eden.everyday.y2021;
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
+import java.util.*;
 
 /**
  * @Description: TODO
@@ -176,6 +173,30 @@ public class ArchAPawn202102 {
             end++;
         }
         return maxLength;
+    }
+
+    /**
+     * @Description: 1423. 可获得的最大点数
+     * @Author: gexx
+     * @Date: 2021/2/6
+     **/
+    public int maxScore(int[] cardPoints, int k) {
+        int n = cardPoints.length;
+        // 滑动窗口大小为 n-k
+        int windowSize = n - k;
+        // 选前 n-k 个作为初始值
+        int sum = 0;
+        for (int i = 0; i < windowSize; ++i) {
+            sum += cardPoints[i];
+        }
+        int minSum = sum;
+        for (int i = windowSize; i < n; ++i) {
+            // 滑动窗口每向右移动一格，增加从右侧进入窗口的元素值，并减少从左侧离开窗口的元素值
+            sum += cardPoints[i] - cardPoints[i - windowSize];
+            minSum = Math.min(minSum, sum);
+        }
+        return Arrays.stream(cardPoints).sum() - minSum;
+
     }
 
     public static void main(String[] args) {
