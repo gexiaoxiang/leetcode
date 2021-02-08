1152cf95b0ee4342eb7d00b378f15da53a2837b6
Apollo2016926 <944644308@qq.com>
2021-02-08 20:54:07 +0800

    将整数转换为两个无零整数的和

--- src/main/java/com/eden/questionbank/algorithms/simple/SimpleFourthPage.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/SimpleFourthPage.java
@@ -19,7 +19,7 @@ public class SimpleFourthPage {
         List<Integer> list = new ArrayList();
         for (int i = 0; i < nums.length - 1; i += 2) {
             for (int i1 = 0; i1 < nums[i]; i1++) {
-                list.add(nums[i+1]);
+                list.add(nums[i + 1]);
             }
         }
         int result[] = new int[list.size()];
@@ -28,6 +28,22 @@ public class SimpleFourthPage {
         }
         return result;
     }
+
+    /**
+     * @Description 1317. 将整数转换为两个无零整数的和
+     * @author gexx
+     * @Date 2021/2/8
+     **/
+    public int[] getNoZeroIntegers(int n) {
+        int[] rs = new int[2];
+        for (int i = 1; i < n; i++) {
+            if (!String.valueOf(i).contains("0") && !String.valueOf(n - i).contains("0")) {
+                return new int[]{i, n - i};
+            }
+        }
+        return rs;
+    }
+
 
     public static void main(String[] args) {
         decompressRLElist(new int[]{1, 2, 3, 4});
