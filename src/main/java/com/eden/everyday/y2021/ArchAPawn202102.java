7e199366e765242c07ec140bd33eb4e15a3dafa3
Apollo2016926 <944644308@qq.com>
2021-02-09 21:08:47 +0800

    最多包含 K 个不同整数的子区间的个数

--- src/main/java/com/eden/everyday/y2021/ArchAPawn202102.java
+++ src/main/java/com/eden/everyday/y2021/ArchAPawn202102.java
@@ -245,6 +245,51 @@ public class ArchAPawn202102 {
         return r - l;
     }
 
+    /**
+     * @Description 992. K 个不同整数的子数组
+     * @author gexx
+     * @Date 2021/2/9
+     **/
+
+    public int subarraysWithKDistinct(int[] A, int K) {
+        return atMostKDistinct(A, K) - atMostKDistinct(A, K - 1);
+    }
+
+    /**
+     * @Description 最多包含 K 个不同整数的子区间的个数
+     * @author gexx
+     * @Date 2021/2/9
+     **/
+    private int atMostKDistinct(int[] A, int K) {
+        int len = A.length;
+        int[] freq = new int[len + 1];
+
+        int left = 0;
+        int right = 0;
+        // [left, right) 里不同整数的个数
+        int count = 0;
+        int res = 0;
+        // [left, right) 包含不同整数的个数小于等于 K
+        while (right < len) {
+            if (freq[A[right]] == 0) {
+                count++;
+            }
+            freq[A[right]]++;
+            right++;
+
+            while (count > K) {
+                freq[A[left]]--;
+                if (freq[A[left]] == 0) {
+                    count--;
+                }
+                left++;
+            }
+            // [left, right) 区间的长度就是对结果的贡献
+            res += right - left;
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
 
         characterReplacement("AABBBCBB", 1);
