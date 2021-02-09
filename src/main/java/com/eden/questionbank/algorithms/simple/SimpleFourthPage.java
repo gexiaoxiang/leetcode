67e21ae46f2a66a7b642a1900210c044680bcb9a
Apollo2016926 <944644308@qq.com>
2021-02-09 21:36:14 +0800

    数组序号转换

--- src/main/java/com/eden/questionbank/algorithms/simple/SimpleFourthPage.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/SimpleFourthPage.java
@@ -1,7 +1,6 @@
 package com.eden.questionbank.algorithms.simple;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -108,6 +107,36 @@ public class SimpleFourthPage {
         return ans;
     }
 
+
+    /**
+     * @Description 1331. 数组序号转换
+     * @author gexx
+     * @Date 2021/2/9
+     **/
+    public int[] arrayRankTransform(int[] arr) {
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for (int num : arr) {
+            if (num < min) min = num;
+            if (num > max) max = num;
+        }
+
+        int[] count = new int[max - min + 1];
+        for (int num : arr)
+            count[num - min] = 1;
+
+        int[] preSum = new int[count.length + 1];
+        for (int i = 1; i < preSum.length; i++)
+            preSum[i] = preSum[i - 1] + count[i - 1];
+
+        int[] ans = new int[arr.length];
+        int index = 0;
+        for (int num : arr)
+            ans[index++] = preSum[num - min] + 1;
+
+        return ans;
+
+
+    }
 
     public static void main(String[] args) {
         decompressRLElist(new int[]{1, 2, 3, 4});
