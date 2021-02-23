d6ac191500c5170ec685d77a0ababb772d0903ca
Apollo2016926 <944644308@qq.com>
2021-02-23 16:49:49 +0800

    1052. 爱生气的书店老板

--- src/main/java/com/eden/everyday/y2021/ArchAPawn202102.java
+++ src/main/java/com/eden/everyday/y2021/ArchAPawn202102.java
@@ -353,6 +353,31 @@ public class ArchAPawn202102 {
         return ret;
     }
 
+    /**
+     * @Description: 1052. 爱生气的书店老板
+     * @Author: gexx
+     * @Date: 2021/2/23
+     **/
+    public int maxSatisfied(int[] customers, int[] grumpy, int X) {
+        int total = 0;
+        int n = customers.length;
+        for (int i = 0; i < n; i++) {
+            if (grumpy[i] == 0) {
+                total += customers[i];
+            }
+        }
+        int increase = 0;
+        for (int i = 0; i < X; i++) {
+            increase += customers[i] * grumpy[i];
+        }
+        int maxIncrease = increase;
+        for (int i = X; i < n; i++) {
+            increase = increase - customers[i - X] * grumpy[i - X] + customers[i] * grumpy[i];
+            maxIncrease = Math.max(maxIncrease, increase);
+        }
+        return total + maxIncrease;
+    }
+
     public static void main(String[] args) {
 
         characterReplacement("AABBBCBB", 1);
