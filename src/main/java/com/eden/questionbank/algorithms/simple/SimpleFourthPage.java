ea6b8279c0a6922ecf43a9ad0ceb054e957c3867
Apollo2016926 <944644308@qq.com>
2021-02-26 14:41:30 +0800

    1480. 一维数组的动态和

--- src/main/java/com/eden/questionbank/algorithms/simple/SimpleFourthPage.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/SimpleFourthPage.java
@@ -728,6 +728,22 @@ public class SimpleFourthPage {
         return result;
     }
 
+    /**
+     * @Description: 1480. 一维数组的动态和
+     * @Author: gexx
+     * @Date: 2021/2/26
+     **/
+    public int[] runningSum(int[] nums) {
+
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            nums[i] = sum;
+        }
+        return nums;
+    }
+
+
     public static void main(String[] args) {
         finalPrices(new int[]{8, 4, 6, 2, 3});
         kLengthApart(new int[]{1, 0, 0, 1, 0, 1}, 2);
