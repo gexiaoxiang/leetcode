d220d35a860fd8fbe25049e3ed8f650fbc0ff863
Apollo2016926 <944644308@qq.com>
2021-03-02 16:16:32 +0800

    1544. 整理字符串

--- src/main/java/com/eden/questionbank/algorithms/simple/SimpleFourthPage.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/SimpleFourthPage.java
@@ -827,6 +827,31 @@ public class SimpleFourthPage {
         return k + n;
     }
 
+    /**
+     * @Description: 1544. 整理字符串
+     * @Author: gexx
+     * @Date: 2021/3/2
+     **/
+    public String makeGood(String s) {
+
+
+        StringBuffer ret = new StringBuffer();
+        int retIndex = -1;
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            char ch = s.charAt(i);
+            if (ret.length() > 0 && Character.toLowerCase(ret.charAt(retIndex)) == Character.toLowerCase(ch) && ret.charAt(retIndex) != ch) {
+                ret.deleteCharAt(retIndex);
+                retIndex--;
+            } else {
+                ret.append(ch);
+                retIndex++;
+            }
+        }
+        return ret.toString();
+
+    }
+
     public static void main(String[] args) {
         findKthPositive(new int[]{2, 3, 4, 7, 11}, 5);
         restoreString("aiohn", new int[]{3, 1, 4, 2, 0});
