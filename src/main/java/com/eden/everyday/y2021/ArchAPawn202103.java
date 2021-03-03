ad7cc92098d1912465e6691ace4b371c32a2ebb3
Apollo2016926 <944644308@qq.com>
2021-03-03 10:28:03 +0800

    338. 比特位计数

--- src/main/java/com/eden/everyday/y2021/ArchAPawn202103.java
+++ src/main/java/com/eden/everyday/y2021/ArchAPawn202103.java
@@ -7,4 +7,23 @@ package com.eden.everyday.y2021;
  * @Version V1.0
  **/
 public class ArchAPawn202103 {
+    /**
+     * @Description: 338. 比特位计数
+     * @Author: gexx
+     * @Date: 2021/3/3
+     **/
+    public int[] countBits(int num) {
+        int[] ints = new int[num + 1];
+        for (int i = 0; i <= num; i++) {
+            String s = Integer.toBinaryString(i);
+            int count = 0;
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(j) == '1') {
+                    count++;
+                }
+            }
+            ints[i] = count;
+        }
+        return ints;
+    }
 }
