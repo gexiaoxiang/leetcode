9f0453d7b71f0fdd351a5a460fc029cf446a137b
Apollo2016926 <944644308@qq.com>
2021-03-05 16:10:07 +0800

    232. 用栈实现队列

--- src/main/java/com/eden/everyday/y2021/ArchAPawn202103.java
+++ src/main/java/com/eden/everyday/y2021/ArchAPawn202103.java
@@ -1,5 +1,8 @@
 package com.eden.everyday.y2021;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * @Description: 每日一题
  * @Author gexx
@@ -25,5 +28,48 @@ public class ArchAPawn202103 {
             ints[i] = count;
         }
         return ints;
+    }
+
+    /**
+     * @Description: 232. 用栈实现队列
+     * @Author: gexx
+     * @Date: 2021/3/5
+     **/
+    class MyQueue {
+        Deque<Integer> inStack;
+        Deque<Integer> outStack;
+
+        public MyQueue() {
+            inStack = new LinkedList<Integer>();
+            outStack = new LinkedList<Integer>();
+        }
+
+        public void push(int x) {
+            inStack.push(x);
+        }
+
+        public int pop() {
+            if (outStack.isEmpty()) {
+                in2out();
+            }
+            return outStack.pop();
+        }
+
+        public int peek() {
+            if (outStack.isEmpty()) {
+                in2out();
+            }
+            return outStack.peek();
+        }
+
+        public boolean empty() {
+            return inStack.isEmpty() && outStack.isEmpty();
+        }
+
+        private void in2out() {
+            while (!inStack.isEmpty()) {
+                outStack.push(inStack.pop());
+            }
+        }
     }
 }
