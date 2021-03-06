9ddfe6ceaf17558c6f898558847c8dab77c7e3c9
Apollo2016926 <944644308@qq.com>
2021-03-06 18:48:31 +0800

    1603. 设计停车系统

--- src/main/java/com/eden/questionbank/algorithms/simple/SimpleFourthPage.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/SimpleFourthPage.java
@@ -1076,6 +1076,47 @@ public class SimpleFourthPage {
         return level;
     }
 
+
+    /**
+     * @Description 1603. 设计停车系统
+     * @Author gexx
+     * @Date 2021/3/6
+     **/
+    class ParkingSystem {
+        private int big;
+        private int medium;
+        private int small;
+
+        public ParkingSystem(int big, int medium, int small) {
+            this.big = big;
+            this.medium = medium;
+            this.small = small;
+        }
+
+        public boolean addCar(int carType) {
+            switch (carType) {
+                case 1: {
+                    big--;
+
+                    return big >= 0;
+                }
+                case 2: {
+                    medium--;
+                    return medium >= 0;
+                }
+                case 3: {
+                    small--;
+                    return small >= 0;
+                }
+                default: {
+                    return false;
+                }
+
+            }
+
+        }
+    }
+
     public static void main(String[] args) {
         String[] s = "js  jj   ss".split(" ");
         System.out.println(Arrays.toString(s));
