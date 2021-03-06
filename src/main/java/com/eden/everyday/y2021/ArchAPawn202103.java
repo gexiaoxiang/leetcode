5e9f35b80927bfb5a2784972d55c3346fb17ba30
Apollo2016926 <944644308@qq.com>
2021-03-06 18:25:39 +0800

    下一个更大元素 II

--- src/main/java/com/eden/everyday/y2021/ArchAPawn202103.java
+++ src/main/java/com/eden/everyday/y2021/ArchAPawn202103.java
@@ -1,5 +1,6 @@
 package com.eden.everyday.y2021;
 
+import java.util.Arrays;
 import java.util.Deque;
 import java.util.LinkedList;
 
@@ -71,5 +72,25 @@ public class ArchAPawn202103 {
                 outStack.push(inStack.pop());
             }
         }
+    }
+
+
+    /**
+     * @Description 下一个更大元素 II
+     * @author gexx
+     * @Date 2021/3/6
+     **/
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] ret = new int[n];
+        Arrays.fill(ret, -1);
+        Deque<Integer> stack = new LinkedList<Integer>();
+        for (int i = 0; i < n * 2 - 1; i++) {
+            while (!stack.isEmpty() && nums[stack.peek()] < nums[i % n]) {
+                ret[stack.pop()] = nums[i % n];
+            }
+            stack.push(i % n);
+        }
+        return ret;
     }
 }
