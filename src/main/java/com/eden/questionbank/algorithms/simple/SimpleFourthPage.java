7c8b8c0a293624b35ba5a62043125f5fc37cf074
Apollo2016926 <944644308@qq.com>
2021-03-10 10:47:21 +0800

    1652. 拆炸弹

--- src/main/java/com/eden/questionbank/algorithms/simple/SimpleFourthPage.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/SimpleFourthPage.java
@@ -1302,6 +1302,36 @@ public class SimpleFourthPage {
         return max;
     }
 
+    /**
+     * @Description: 1652. 拆炸弹
+     * @Author: gexx
+     * @Date: 2021/3/10
+     **/
+    public int[] decrypt(int[] code, int k) {
+        int n = code.length;
+        int[] result = new int[code.length];
+        if (k > 0) {
+            int sum = 0;
+            for (int i = 0; i < k; i++) {
+                sum += code[i];
+            }
+            for (int i = 0; i < n; i++) {
+                result[i] = sum - code[i] + code[(i + k) % n];
+                sum = result[i];
+            }
+        } else if (k < 0) {
+            int sum = 0;
+            for (int i = 0; i < -k; i++) {
+                sum += code[i];
+            }
+            for (int i = -k + 1; i < n - k + 1; i++) {
+                result[i % n] = sum - code[(i + k - 1) % n] + code[(i - 1) % n];
+                sum = result[i % n];
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         System.out.println(getMaximumGenerated(100));
         slowestKey(new int[]{12, 23, 36, 46, 62}, "spuda");
