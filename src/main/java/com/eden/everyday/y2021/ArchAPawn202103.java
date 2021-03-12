28d8be6bc2ff2c4700bd5827656fda52f172e971
Apollo2016926 <944644308@qq.com>
2021-03-12 11:15:27 +0800

    331. 验证二叉树的前序序列化

--- src/main/java/com/eden/everyday/y2021/ArchAPawn202103.java
+++ src/main/java/com/eden/everyday/y2021/ArchAPawn202103.java
@@ -213,6 +213,44 @@ public class ArchAPawn202103 {
         return sum;
     }
 
+    /**
+     * @Description: 331. 验证二叉树的前序序列化
+     * @Author: gexx
+     * @Date: 2021/3/12
+     **/
+    public boolean isValidSerialization(String preorder) {
+        int n = preorder.length();
+        int i = 0;
+        Deque<Integer> stack = new LinkedList<Integer>();
+        stack.push(1);
+        while (i < n) {
+            if (stack.isEmpty()) {
+                return false;
+            }
+            if (preorder.charAt(i) == ',') {
+                i++;
+            } else if (preorder.charAt(i) == '#') {
+                int top = stack.pop() - 1;
+                if (top > 0) {
+                    stack.push(top);
+                }
+                i++;
+            } else {
+                // 读一个数字
+                while (i < n && preorder.charAt(i) != ',') {
+                    i++;
+                }
+                int top = stack.pop() - 1;
+                if (top > 0) {
+                    stack.push(top);
+                }
+                stack.push(2);
+            }
+        }
+        return stack.isEmpty();
+
+    }
+
     public static void main(String[] args) {
         minCut("ssop");
         calculate2("3+2*2");
