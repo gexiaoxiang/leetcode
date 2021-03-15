2d7491a922c20620b23735c91bc9ce62c8e8f8a9
Apollo2016926 <944644308@qq.com>
2021-03-15 09:34:28 +0800

    1716. 计算力扣银行的钱

--- src/main/java/com/eden/questionbank/algorithms/simple/SimpleFourthPage.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/SimpleFourthPage.java
@@ -1469,6 +1469,16 @@ public class SimpleFourthPage {
         return count;
     }
 
+    /**
+     * @Description: 1716. 计算力扣银行的钱
+     * @Author: gexx
+     * @Date: 2021/3/15
+     **/
+    public int totalMoney(int n) {
+        int day = n % 7, week = n / 7;
+        return ((day + 1) * day) / 2 + day * week + (((week + 7) * week) / 2) * 7;
+    }
+
     public static void main(String[] args) {
         interpret("G()(al)");
         maxLengthBetweenEqualCharacters("scayofdzca");
