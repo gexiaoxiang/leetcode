ef780211b20f93de2715b8effb045fdf6bac4176
Apollo2016926 <944644308@qq.com>
2021-03-16 11:09:00 +0800

    59. 螺旋矩阵 II

--- src/main/java/com/eden/everyday/y2021/ArchAPawn202103.java
+++ src/main/java/com/eden/everyday/y2021/ArchAPawn202103.java
@@ -274,6 +274,31 @@ public class ArchAPawn202103 {
         }
     }
 
+    /**
+     * @Description: 59. 螺旋矩阵 II
+     * @Author: gexx
+     * @Date: 2021/3/16
+     **/
+    public int[][] generateMatrix(int n) {
+        int[][] arr = new int[n][n];
+        int c = 1, j = 0;
+        while (c <= n * n) {
+
+            for (int i = j; i < n - j; i++)
+                arr[j][i] = c++;
+            for (int i = j + 1; i < n - j; i++)
+                arr[i][n - j - 1] = c++;
+            for (int i = n - j - 2; i >= j; i--)
+                arr[n - j - 1][i] = c++;
+            for (int i = n - j - 2; i > j; i--)
+                arr[i][j] = c++;
+
+            j++;
+        }
+
+        return arr;
+    }
+
     public static void main(String[] args) {
         minCut("ssop");
         calculate2("3+2*2");
