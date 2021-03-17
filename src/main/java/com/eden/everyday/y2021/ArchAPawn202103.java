1ed5653fce5bcaa0e859d760ffe57700196daac4
Apollo2016926 <944644308@qq.com>
2021-03-17 15:52:50 +0800

    115. 不同的子序列

--- src/main/java/com/eden/everyday/y2021/ArchAPawn202103.java
+++ src/main/java/com/eden/everyday/y2021/ArchAPawn202103.java
@@ -299,6 +299,30 @@ public class ArchAPawn202103 {
         return arr;
     }
 
+
+    public int numDistinct(String s, String t) {
+        int m = s.length(), n = t.length();
+        if (m < n) {
+            return 0;
+        }
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            dp[i][n] = 1;
+        }
+        for (int i = m - 1; i >= 0; i--) {
+            char sChar = s.charAt(i);
+            for (int j = n - 1; j >= 0; j--) {
+                char tChar = t.charAt(j);
+                if (sChar == tChar) {
+                    dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j];
+                } else {
+                    dp[i][j] = dp[i + 1][j];
+                }
+            }
+        }
+        return dp[0][0];
+    }
+
     public static void main(String[] args) {
         minCut("ssop");
         calculate2("3+2*2");
