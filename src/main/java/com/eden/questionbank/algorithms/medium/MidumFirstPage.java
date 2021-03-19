b5acfd95c13058b5896ba3e836e39629f9f50393
Apollo2016926 <944644308@qq.com>
2021-03-19 10:57:54 +0800

    31. 下一个排列

--- src/main/java/com/eden/questionbank/algorithms/medium/MidumFirstPage.java
+++ src/main/java/com/eden/questionbank/algorithms/medium/MidumFirstPage.java
@@ -37,5 +37,64 @@ public class MidumFirstPage {
         for (StringBuilder row : rows) ret.append(row);
         return ret.toString();
     }
+
+    static int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    static String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+    /**
+     * @Description: 12. 整数转罗马数字
+     * @Author: gexx
+     * @Date: 2021/3/19
+     **/
+    public static String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < values.length && num >= 0; i++) {
+            while (values[i] <= num) {
+                num -= values[i];
+                sb.append(symbols[i]);
+            }
+        }
+        return sb.toString();
+
+    }
+
+    /**
+     * @Description: 31. 下一个排列
+     * @Author: gexx
+     * @Date: 2021/3/19
+     **/
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[i] >= nums[j]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public void reverse(int[] nums, int start) {
+        int left = start, right = nums.length - 1;
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        intToRoman(66);
+    }
 }
 
