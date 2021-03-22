d3adcdf042569c06878892a39760c27db6b89b56
Apollo2016926 <944644308@qq.com>
2021-03-22 09:40:36 +0800

    39. 组合总和

--- src/main/java/com/eden/questionbank/algorithms/medium/MidumFirstPage.java
+++ src/main/java/com/eden/questionbank/algorithms/medium/MidumFirstPage.java
@@ -93,6 +93,36 @@ public class MidumFirstPage {
         }
     }
 
+    /**
+     * @Description: 39. 组合总和
+     * @Author: gexx
+     * @Date: 2021/3/22
+     **/
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        List<Integer> combine = new ArrayList<Integer>();
+        dfs(candidates, target, ans, combine, 0);
+        return ans;
+    }
+
+    public void dfs(int[] candidates, int target, List<List<Integer>> ans, List<Integer> combine, int idx) {
+        if (idx == candidates.length) {
+            return;
+        }
+        if (target == 0) {
+            ans.add(new ArrayList<Integer>(combine));
+            return;
+        }
+        // 直接跳过
+        dfs(candidates, target, ans, combine, idx + 1);
+        // 选择当前数
+        if (target - candidates[idx] >= 0) {
+            combine.add(candidates[idx]);
+            dfs(candidates, target - candidates[idx], ans, combine, idx);
+            combine.remove(combine.size() - 1);
+        }
+    }
+
     public static void main(String[] args) {
         intToRoman(66);
     }
