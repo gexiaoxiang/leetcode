45b3efa2f4b046f39b40eae1a8f4cb9aa31aac75
Apollo2016926 <944644308@qq.com>
2021-03-30 10:28:50 +0800

    74. 搜索二维矩阵

--- src/main/java/com/eden/everyday/y2021/ArchAPawn202103.java
+++ src/main/java/com/eden/everyday/y2021/ArchAPawn202103.java
@@ -323,7 +323,29 @@ public class ArchAPawn202103 {
         return dp[0][0];
     }
 
+    /**
+     * @Description: 74. 搜索二维矩阵
+     * @Author: gexx
+     * @Date: 2021/3/30
+     **/
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] <= target && target <= matrix[i][n - 1]) {
+                for (int matrix1 : matrix[i]) {
+                    if (target == matrix1) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) {
+        searchMatrix(new int[][]{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}}, 3);
         minCut("ssop");
         calculate2("3+2*2");
     }
