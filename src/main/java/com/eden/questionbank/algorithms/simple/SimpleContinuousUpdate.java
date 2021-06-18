35f58657823af9e8a2664f63da9b77b87026cbd8
Apollo2016926 <944644308@qq.com>
2021-06-18 18:02:55 +0800

    字符串中第二大的数字

--- src/main/java/com/eden/questionbank/algorithms/simple/SimpleContinuousUpdate.java
+++ src/main/java/com/eden/questionbank/algorithms/simple/SimpleContinuousUpdate.java
@@ -1,5 +1,9 @@
 package com.eden.questionbank.algorithms.simple;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @Description: 简单题持续更新
  * @Author gexx
@@ -24,5 +28,36 @@ public class SimpleContinuousUpdate {
         return total + duration;
     }
 
+    /**
+     * @Description: 1796. 字符串中第二大的数字
+     * @Author: gexx
+     * @Date: 2021/6/18
+     **/
+    public static int secondHighest(String s) {
+        List<Integer> list = new ArrayList<>();
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                list.add(Integer.valueOf(String.valueOf(c)));
+            }
+        }
+
+        int[] re = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            re[i] = list.get(i);
+        }
+        Arrays.sort(re);
+        int[] ints = Arrays.stream(re).distinct().toArray();
+        if (ints.length <= 1) return -1;
+        if (ints[ints.length - 1] > ints[ints.length - 2]) {
+            return ints[ints.length - 2];
+        }
+
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        secondHighest("ck077");
+    }
 
 }
