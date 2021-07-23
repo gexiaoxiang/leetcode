11d0cc752ae04d4813bb80a8de56d4a08075aec0
Apollo2016926 <944644308@qq.com>
2021-07-23 10:52:15 +0800

    1886. 判断矩阵经轮转后是否一致

--- src/main/java/com/eden/everyday/y2021/ArchAPawn202107.java
+++ src/main/java/com/eden/everyday/y2021/ArchAPawn202107.java
@@ -110,4 +110,37 @@ public class ArchAPawn202107 {
 
         return true;
     }
+
+    /**
+     * 1886. 判断矩阵经轮转后是否一致
+     *
+     * @param mat
+     * @param target
+     * @return
+     */
+    public boolean findRotation(int[][] mat, int[][] target) {
+        int n = mat.length;
+        boolean b1 = true, b2 = true, b3 = true, b4 = true;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                //旋转90度
+                if (mat[n - j - 1][i] != target[i][j]) {
+                    b1 = false;
+                }
+                //旋转180度
+                if (mat[n - i - 1][n - j - 1] != target[i][j]) {
+                    b2 = false;
+                }
+                //旋转270度
+                if (mat[j][n - i - 1] != target[i][j]) {
+                    b3 = false;
+                }
+                //旋转360度
+                if (mat[i][j] != target[i][j]) {
+                    b4 = false;
+                }
+            }
+        }
+        return b1 || b2 || b3 || b4;
+    }
 }
