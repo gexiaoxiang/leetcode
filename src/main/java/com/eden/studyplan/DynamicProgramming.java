1e91eb81fb82712328e89eaadf8b0e75affa3bc1
Apollo2016926 <944644308@qq.com>
2021-07-28 13:40:35 +0800

    509. 斐波那契数

--- src/main/java/com/eden/studyplan/DynamicProgramming.java
+++ src/main/java/com/eden/studyplan/DynamicProgramming.java
@@ -23,6 +23,28 @@ public class DynamicProgramming {
             r = p + q;
         }
         return r;
-
     }
+
+    /**
+     * @Description: 1137. 第 N 个泰波那契数
+     * @Author: gexx
+     * @Date: 2021/7/28
+     **/
+    public int tribonacci(int n) {
+        if (n == 0) {
+            return n;
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        int tmp, x = 0, y = 1, z = 1;
+        for (int i = 3; i <= n; ++i) {
+            tmp = x + y + z;
+            x = y;
+            y = z;
+            z = tmp;
+        }
+        return z;
+    }
+
 }
