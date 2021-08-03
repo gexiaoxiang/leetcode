ea8ff20757daf18d60ff5ee19ed651e96da27dad
Apollo2016926 <944644308@qq.com>
2021-08-03 14:57:47 +0800

    63. 不同路径 II

--- src/main/java/com/eden/questionbank/algorithms/medium/MidumFirstPage.java
+++ src/main/java/com/eden/questionbank/algorithms/medium/MidumFirstPage.java
@@ -222,6 +222,31 @@ public class MidumFirstPage {
         return ans;
     }
 
+    /**
+     * @Description: 63. 不同路径 II
+     * @Author: gexx
+     * @Date: 2021/8/3
+     **/
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int n = obstacleGrid.length, m = obstacleGrid[0].length;
+        int[] f = new int[m];
+
+        f[0] = obstacleGrid[0][0] == 0 ? 1 : 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (obstacleGrid[i][j] == 1) {
+                    f[j] = 0;
+                    continue;
+                }
+                if (j - 1 >= 0 && obstacleGrid[i][j - 1] == 0) {
+                    f[j] += f[j - 1];
+                }
+            }
+        }
+
+        return f[m - 1];
+
+    }
 
     public static void main(String[] args) {
         insert(new int[][]{{1, 3}, {6, 9}}, new int[]{2, 5});
