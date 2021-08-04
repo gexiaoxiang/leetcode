40ca2b9013a85019bc2fdacac5d9ff5325354d6d
Apollo2016926 <944644308@qq.com>
2021-08-04 16:52:57 +0800

    611. 有效三角形的个数

--- src/main/java/com/eden/everyday/y2021/ArchAPawn202108.java
+++ src/main/java/com/eden/everyday/y2021/ArchAPawn202108.java
@@ -1,6 +1,8 @@
 package com.eden.everyday.y2021;
 
 
+import java.util.Arrays;
+
 /**
  * @Description: 八月
  * @Author gexx
@@ -8,6 +10,28 @@ package com.eden.everyday.y2021;
  * @Version V1.0
  **/
 public class ArchAPawn202108 {
+    /**
+     * 611. 有效三角形的个数
+     *
+     * @param nums
+     * @return
+     */
+    public int triangleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length - 2; k++) {
 
+                    if (nums[i] + nums[j] > nums[k]) {
+                        count++;
+                    }
+                }
+            }
+        }
+
+
+        return count;
+    }
 
 }
